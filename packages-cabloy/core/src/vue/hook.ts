b05b64e449d3e38ff94201de8e067c0ecc4a8802
zhennann <zhen.nann@icloud.com>
2024-04-17 09:25:41 +0800

    Update hook.ts

--- packages-cabloy/core/src/vue/hook.ts
+++ packages-cabloy/core/src/vue/hook.ts
@@ -10,10 +10,10 @@ export type ExtractHook<Hook> = {
         : Hook[Prop];
 };
 
-export type ReturnTypeHook<Hook extends (...args: any) => any> = Hook extends (...args: any) => infer R
+export type ReturnTypeHook<Hook extends (...args: any[]) => any> = Hook extends (...args: any[]) => infer R
   ? ExtractHook<R>
   : any;
 
-export function useHook<T extends (...args: any) => any>(hook: T, ...args: Parameters<T>): ReturnTypeHook<T> {
+export function useHook<T extends (...args: any[]) => any>(hook: T, ...args: Parameters<T>): ReturnTypeHook<T> {
   return hook(...args);
 }
