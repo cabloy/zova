d91943fc83ee6839469898436139f9949b9881b2
zhennann <zhen.nann@icloud.com>
2024-04-23 11:36:20 +0800

    Update router.ts

--- packages-cabloy/core/src/core/component/router.ts
+++ packages-cabloy/core/src/core/component/router.ts
@@ -1,5 +1,4 @@
 import { Router } from 'vue-router';
-import * as ModuleInfo from '@cabloy/module-info';
 import { BeanSimple } from '../../bean/beanSimple.js';
 import { CabloyApplication } from '../app/index.js';
 
@@ -14,6 +13,10 @@ export class ViewRouter extends BeanSimple {
     return app.bean._newBeanSimple(ViewRouter, false) as ViewRouterLike;
   }
 
+  // get router() {
+  //   return this[SymbolRouter];
+  // }
+
   protected __get__(prop) {
     return this[SymbolRouter] && this[SymbolRouter][prop];
   }
@@ -21,29 +24,5 @@ export class ViewRouter extends BeanSimple {
   /** @internal */
   public async initialize(router: Router) {
     this[SymbolRouter] = router;
-    this._routerGuards();
-  }
-
-  private _routerGuards() {
-    this[SymbolRouter].beforeEach(async to => {
-      // fullPath
-      const fullPath = to.fullPath;
-      // // home
-      // if (fullPath === '/') {
-      //   return { path: '/a/home/home' };
-      // }
-      // module info
-      const moduleInfo = ModuleInfo.parseInfo(fullPath);
-      if (!moduleInfo) {
-        // donothing
-        return;
-      }
-      const module = this.app.meta.module.get(moduleInfo.relativeName);
-      if (module) return;
-      // use module
-      await this.app.meta.module.use(fullPath);
-      // redirect again
-      return fullPath;
-    });
   }
 }
