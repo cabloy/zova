8b47f65c6406f337173348bafb5c9d8596ac70e4
zhennann <zhen.nann@icloud.com>
2024-04-26 15:51:35 +0800

    feat: router

--- packages-cabloy/core/src/core/app/application.ts
+++ packages-cabloy/core/src/core/app/application.ts
@@ -5,14 +5,12 @@ import { PluginCabloyOptions } from '../../types/interface/pluginCabloy.js';
 import { CabloyConfig, configDefault } from './config.js';
 import { CabloyConstant, constantDefault } from './constant.js';
 import { PluginBean } from '../../plugins/bean.js';
-import { AppView } from '../component/view.js';
 import { IModuleLocaleText } from '../../bean/resource/locale/type.js';
 
 export class CabloyApplication {
   vue: App;
   bean: BeanContainerLike;
   meta: AppMeta;
-  view: AppView;
   config: CabloyConfig;
   constant: CabloyConstant;
   $text: IModuleLocaleText;
@@ -23,20 +21,17 @@ export class CabloyApplication {
     this.vue = vue;
     this.bean = BeanContainer.create(this, null);
     this.meta = this.bean._newBeanSimple(AppMeta, false);
-    this.view = this.bean._newBeanSimple(AppView, false);
     this.$text = this.meta.locale.createLocaleText();
   }
 
   /** @internal */
-  public async initialize({ modulesMeta, Monkey, locales, config, router }: PluginCabloyOptions) {
+  public async initialize({ modulesMeta, Monkey, locales, config }: PluginCabloyOptions) {
     // monkey
     await this.meta.initialize(Monkey);
     // locales
     await this.meta.locale.initialize(locales);
     // errors
     await this.meta.error.initialize();
-    // router
-    await this.view.initialize(router);
     // config
     this.config = this.meta.util.extend({}, configDefault, config);
     // constant
@@ -50,7 +45,7 @@ export class CabloyApplication {
     // monkey: appInitialized
     await this.meta.module._monkeyModule('appInitialized');
     // monkey: routerGuards
-    await this.meta.module._monkeyModule('routerGuards', undefined, this.view.router);
+    await this.meta.module._monkeyModule('routerGuards', undefined);
   }
 
   private async handlePlugins() {
