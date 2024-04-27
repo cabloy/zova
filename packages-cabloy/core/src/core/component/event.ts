e4b0930144bdb8036bb8b99b9814e49642b07b6c
zhennann <zhen.nann@icloud.com>
2024-04-27 18:53:39 +0800

    feat: event emit

--- packages-cabloy/core/src/core/component/event.ts
+++ packages-cabloy/core/src/core/component/event.ts
@@ -1,7 +1,42 @@
+import { composeAsync } from '@cabloy/compose';
 import { BeanSimple } from '../../bean/beanSimple.js';
+import { IEventRecord, IEventResultRecord, TypeEventHandlers, TypeEventNext } from '../../types/interface/event.js';
+
+const __adapter = (_context, chain) => {
+  const eventHandler = chain;
+  return {
+    receiver: undefined,
+    fn: eventHandler,
+  };
+};
 
 export class AppEvent extends BeanSimple {
-  private;
+  private eventHandlersMap: Record<string, TypeEventHandlers> = {};
+
   /** @internal */
   public async initialize() {}
+
+  async emit<K extends keyof IEventRecord>(
+    eventName: K,
+    data: IEventRecord[K],
+    result: IEventResultRecord[K],
+    next: TypeEventNext<IEventRecord[K], IEventResultRecord[K]>,
+  ): Promise<IEventResultRecord[K]> {
+    const eventHandlers = this.eventHandlersMap[eventName];
+    // context
+    const context = {
+      data,
+      result,
+    };
+    // invoke
+    await composeAsync(eventHandlers, __adapter)(context, async (context, _next) => {
+      if (next) {
+        await next(context, _next);
+      } else {
+        await _next();
+      }
+    });
+    // ok
+    return context.result;
+  }
 }
--- packages-cabloy/core/src/types/interface/event.ts
+++ packages-cabloy/core/src/types/interface/event.ts
@@ -1,2 +1,16 @@
+export type TypeEventHandlers<D = unknown, R = unknown> = TypeEventNext<D, R>[];
+
+export type TypeEventContext<D, R> = {
+  data: D;
+  result: R;
+};
+
+export type TypeEventNext<D, R> = {
+  (context: TypeEventContext<D, R>, next: TypeEventNext<D, R>): Promise<void>;
+};
+
 export interface IEventRecord {}
 export type TypeEventRecordKeys = keyof IEventRecord;
+
+export interface IEventResultRecord {}
+export type TypeEventResultRecordKeys = keyof IEventResultRecord;
