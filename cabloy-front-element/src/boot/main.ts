3ed5aed4c7f37a330984e49e4776c1e50e5bab2a
zhennann <zhen.nann@icloud.com>
2024-04-30 23:13:16 +0800

    feat: pacakge

--- cabloy-front-element/src/boot/main.ts
+++ cabloy-front-element/src/boot/main.ts
@@ -1,9 +1,3 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
 import { createApp } from 'vue';
 import 'roboto-fontface/css/roboto/sass/roboto-fontface.scss';
 import vuetify from './vuetify.js';
