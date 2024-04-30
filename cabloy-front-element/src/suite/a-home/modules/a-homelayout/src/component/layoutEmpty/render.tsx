e023564415fcfa65913e7cde8888c614ccafdd54
zhennann <zhen.nann@icloud.com>
2024-04-30 22:58:03 +0800

    feat: deps

--- cabloy-front-element/src/suite/a-home/modules/a-homelayout/src/component/layoutEmpty/render.tsx
+++ cabloy-front-element/src/suite/a-home/modules/a-homelayout/src/component/layoutEmpty/render.tsx
@@ -1,18 +1,13 @@
 import { BeanRenderBase, Local } from '@cabloy/front-core';
 import type { MotherLayoutEmpty } from './mother.js';
-import { VApp, VMain } from 'vuetify/components';
+import { ElConfigProvider } from 'element-plus';
 
 export interface RenderLayoutEmpty extends MotherLayoutEmpty { }
 
 @Local()
 export class RenderLayoutEmpty extends BeanRenderBase {
   render() {
-    return (
-      <VApp>
-        <VMain>
-          <router-view />
-        </VMain>
-      </VApp>
-    );
+    return <ElConfigProvider>
+    </ElConfigProvider>
   }
 }
