c5af965a14ea1d544b2a083160c61c87051e26d2
zhennann <zhen.nann@icloud.com>
2024-05-01 20:10:53 +0800

    feat: antdv

--- cabloy-front-antdv/src/suite-vendor/a-vuetify/modules/a-vuetify/src/resource/beans.ts
+++
@@ -1,3 +0,0 @@
-declare module '@cabloy/front-core' {
-  export interface IBeanRecord {}
-}
--- cabloy-front-antdv/src/suite-vendor/a-vuetify/modules/a-vuetify/src/resource/components.ts
+++
@@ -1,3 +0,0 @@
-import { IModuleRouteComponent } from 'cabloy-module-front-a-router';
-
-export const components: Record<string, IModuleRouteComponent> = {};
