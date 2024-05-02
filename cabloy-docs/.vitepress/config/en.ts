f5b06894ea64915aed368071af960c7cd2ec5e81
zhennann <zhen.nann@icloud.com>
2024-05-02 23:01:10 +0800

    feat: locale

--- cabloy-docs/.vitepress/config/en.ts
+++ cabloy-docs/.vitepress/config/en.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitepress';
 export const en = defineConfig({
   lang: 'en-US',
   description: '',
-  themeConfig: {},
+  themeConfig: {
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2016-present zhennann',
+    },
+  },
 });
--- cabloy-docs/.vitepress/config/zh.ts
+++ cabloy-docs/.vitepress/config/zh.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitepress';
 export const zh = defineConfig({
   lang: 'zh-CN',
   description: '',
-  themeConfig: {},
+  themeConfig: {
+    footer: {
+      message: '基于 MIT 许可发布',
+      copyright: `版权所有 © 2016-present 濮水大叔`,
+    },
+  },
 });
