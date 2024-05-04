5a16cb265eae43fac4063861d810f1cc61e02db9
zhennann <zhen.nann@icloud.com>
2024-05-05 06:41:19 +0800

    feat: flavor

--- cabloy-front-core/packages-utils/app-vite/src/utils.ts
+++ cabloy-front-core/packages-utils/app-vite/src/utils.ts
@@ -1,11 +1,14 @@
 import { CabloyConfigMeta } from '@cabloy/front-core';
 import parseArgs from 'minimist';
 
-export function getFlavor(offset: number = 2) {
+export function getFlavor(offset: number = 2): string {
   let flavor = process.env.FLAVOR;
   if (!flavor) {
     const argv = parseArgs(process.argv.slice(offset));
-    flavor = argv.flavor || 'web';
+    flavor = argv.flavor;
+  }
+  if (!flavor) {
+    flavor = 'web';
   }
   return flavor;
 }
