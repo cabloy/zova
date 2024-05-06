5479ea85c80f33c7f5e722e046d620d343d01a2f
zhennann <zhen.nann@icloud.com>
2024-05-06 18:18:45 +0800

    Update shared.ts

--- cabloy-docs/.vitepress/config/shared.ts
+++ cabloy-docs/.vitepress/config/shared.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress';
 export const shared = defineConfig({
   title: 'Cabloy-Front',
   description: 'A vue3 framework with ioc',
-  base: '/cabloy-front/',
+  base: '/',
   themeConfig: {
     editLink: {
       pattern: 'https://github.com/cabloy/cabloy-front/edit/main/cabloy-docs/:path',
