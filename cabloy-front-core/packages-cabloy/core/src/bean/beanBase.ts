3c6f5e73cd3c1c83d3ec1f698ae9453dbd645c19
zhennann <zhen.nann@icloud.com>
2024-05-08 15:21:12 +0800

    Update beanBase.ts

--- cabloy-front-core/packages-cabloy/core/src/bean/beanBase.ts
+++ cabloy-front-core/packages-cabloy/core/src/bean/beanBase.ts
@@ -1,5 +1,4 @@
 import { RendererNode } from 'vue';
-import { Cast } from '../types/utils/cast.js';
 import { BeanBaseSimple } from './beanBaseSimple.js';
 import { IBeanScopeRecord, TypeBeanScopeRecordKeys } from './type.js';
 import { IModuleLocaleText } from './resource/locale/type.js';
@@ -7,22 +6,22 @@ import { AppEvent } from '../core/component/event.js';
 import { getIcon } from './resource/index.js';
 
 export class BeanBase<TScopeModule = unknown> extends BeanBaseSimple {
-  public get $el(): RendererNode {
+  protected get $el(): RendererNode {
     if (!this.ctx) {
       throw new Error('$el can not be used inside global bean.');
     }
     return this.ctx.meta.el;
   }
 
-  public get $text(): IModuleLocaleText {
+  protected get $text(): IModuleLocaleText {
     return this.app.meta.text;
   }
 
-  public get $event(): AppEvent {
+  protected get $event(): AppEvent {
     return this.app.meta.event;
   }
 
-  public get $icon(): typeof getIcon {
+  protected get $icon(): typeof getIcon {
     return getIcon;
   }
 
@@ -44,10 +43,11 @@ export class BeanBase<TScopeModule = unknown> extends BeanBaseSimple {
     return this.app.bean.scope(moduleScope);
   }
 
-  dispose() {
-    const self = Cast(this);
-    if (self.__dispose__) {
-      self.__dispose__();
-    }
-  }
+  // need not
+  // public dispose() {
+  //   const self = Cast(this);
+  //   if (self.__dispose__) {
+  //     self.__dispose__();
+  //   }
+  // }
 }
