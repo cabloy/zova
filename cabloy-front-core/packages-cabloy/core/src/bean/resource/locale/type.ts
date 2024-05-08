f0a8ec6c9d95881e5a181d9433df868be400be01
zhennann <zhen.nann@icloud.com>
2024-05-08 18:18:37 +0800

    Update type.ts

--- cabloy-front-core/packages-cabloy/core/src/bean/resource/locale/type.ts
+++ cabloy-front-core/packages-cabloy/core/src/bean/resource/locale/type.ts
@@ -2,11 +2,14 @@ export interface IModuleLocale {
   (...args: any[]): string;
   locale: (locale: string, ...args: any[]) => string;
 }
+
 export interface IModuleLocaleText {
   (text: string, ...args: any[]): string;
   locale: (locale: string, text: string, ...args: any[]) => string;
 }
 
-export type TypeModuleLocales<T extends { 'zh-cn': object }> = {
-  [prop in string & keyof T['zh-cn']]: IModuleLocale;
+export type TypeModuleLocales<T> = {
+  [prop in string & keyof T]: IModuleLocale;
 };
+
+export type TypeLocaleBase = 'zh-cn';
