5ebbb219b96a45f8bed7ea4455d92767580ebe0e
zhennann <zhen.nann@icloud.com>
2024-05-08 18:25:25 +0800

    feat: locals

---
+++ cabloy-front-core/packages-cabloy/core/src/core/app/locale.ts
@@ -0,0 +1,11 @@
+import { IBeanScopeLocale, PowerPartial } from '../../index.js';
+
+export const localeDefault = {
+  modules: {},
+};
+
+export type CabloyLocale = {
+  modules: IBeanScopeLocale;
+} & typeof localeDefault;
+
+export type CabloyLocaleOptional = PowerPartial<CabloyLocale>;
