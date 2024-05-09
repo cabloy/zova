dc4997f45d0edfd73e2a0b7052ac296b5163f08f
zhennann <zhen.nann@icloud.com>
2024-05-09 17:47:55 +0800

    feat: update

--- cabloy-front-element/src/suite/a-home/modules/a-homepagesystem/src/resource/scope.ts
+++ cabloy-front-element/src/suite/a-home/modules/a-homepagesystem/src/resource/scope.ts
@@ -1,1 +1,29 @@
-/Volumes/my-data/cabloy/cabloy-front/cabloy-front-antdv/prettier.config.cjs
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from '@cabloy/front';
+import { config, Errors, locales, constants } from '../config/index.js';
+import { components } from './components.js';
+
+@Scope()
+export class ScopeModuleAHomepagesystem extends BeanScopeBase {}
+
+export interface ScopeModuleAHomepagesystem
+  extends TypeModuleResource<
+    typeof components,
+    typeof config,
+    typeof Errors,
+    (typeof locales)[TypeLocaleBase],
+    typeof constants
+  > {}
+
+declare module '@cabloy/front-core' {
+  export interface IBeanScopeRecord {
+    'a-homepagesystem': ScopeModuleAHomepagesystem;
+  }
+
+  export interface IBeanScopeConfig {
+    'a-homepagesystem': ReturnType<typeof config>;
+  }
+
+  export interface IBeanScopeLocale {
+    'a-homepagesystem': (typeof locales)[TypeLocaleBase];
+  }
+}
