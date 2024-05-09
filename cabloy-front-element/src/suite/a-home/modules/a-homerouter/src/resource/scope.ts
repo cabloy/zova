2424b23dd46c3f4ae0f101ee51f631d3a0f1b008
zhennann <zhen.nann@icloud.com>
2024-05-09 17:36:46 +0800

    feat: scope components

--- cabloy-front-element/src/suite/a-home/modules/a-homerouter/src/resource/scope.ts
+++ cabloy-front-element/src/suite/a-home/modules/a-homerouter/src/resource/scope.ts
@@ -7,7 +7,12 @@ export class ScopeModuleAHomerouter extends BeanScopeBase {}
 
 export interface ScopeModuleAHomerouter
   extends TypeModuleResource<
-    typeof components,typeof config, typeof Errors, (typeof locales)[TypeLocaleBase], typeof constants> {}
+    typeof components,
+    typeof config,
+    typeof Errors,
+    (typeof locales)[TypeLocaleBase],
+    typeof constants
+  > {}
 
 declare module '@cabloy/front-core' {
   export interface IBeanScopeRecord {
