ea807a4d04fb10c7bbcb7078df9ba85a9b15af1e
zhennann <zhen.nann@icloud.com>
2024-05-12 23:04:32 +0800

    Update beanMotherPageBase.ts

--- cabloy-front-core/packages-cabloy/core/src/bean/beanMotherPageBase.ts
+++ cabloy-front-core/packages-cabloy/core/src/bean/beanMotherPageBase.ts
@@ -2,9 +2,9 @@ import { BeanBase } from './beanBase.js';
 import { IMotherData } from './type.js';
 
 export class BeanMotherPageBase<
-  TScopeModule = unknown,
   Params = unknown,
   Query = unknown,
+  TScopeModule = unknown,
 > extends BeanBase<TScopeModule> {
   public $params: Params;
   public $query: Query;
