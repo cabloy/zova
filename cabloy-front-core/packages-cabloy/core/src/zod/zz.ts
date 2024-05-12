f70d3418fc8d508f5e242bf61095cc7db8db69d9
zhennann <zhen.nann@icloud.com>
2024-05-12 22:27:58 +0800

    Update zz.ts

--- cabloy-front-core/packages-cabloy/core/src/zod/zz.ts
+++ cabloy-front-core/packages-cabloy/core/src/zod/zz.ts
@@ -1,13 +1,5 @@
 import { RawCreateParams, z, ZodRawShape, ZodType, ZodTypeAny } from 'zod';
 
-// array/object
-export function array<T extends ZodTypeAny>(schema: T, params?: RawCreateParams) {
-  return z.array(schema, params);
-}
-export function object<T extends ZodRawShape>(shape: T, params?: RawCreateParams) {
-  return z.object(shape, params);
-}
-
 // coerce
 export function string(...args: Parameters<typeof z.coerce.string>) {
   return z.coerce.string(...args);
@@ -15,18 +7,18 @@ export function string(...args: Parameters<typeof z.coerce.string>) {
 export function number(...args: Parameters<typeof z.coerce.number>) {
   return z.coerce.number(...args);
 }
-export function boolean(...args: Parameters<typeof z.coerce.boolean>) {
-  return z.coerce.boolean(...args);
-}
 export function bigint(...args: Parameters<typeof z.coerce.bigint>) {
   return z.coerce.bigint(...args);
 }
 export function date(...args: Parameters<typeof z.coerce.date>) {
   return z.coerce.date(...args);
 }
+// export function boolean(...args: Parameters<typeof z.coerce.boolean>) {
+//   return z.coerce.boolean(...args);
+// }
 
-// bool
-export function bool(...args: Parameters<typeof z.boolean>) {
+// boolean
+export function boolean(...args: Parameters<typeof z.boolean>) {
   return z.preprocess(
     val => {
       if (val === undefined) return undefined;
@@ -48,6 +40,24 @@ export function json<T extends ZodRawShape>(shape: T, params?: RawCreateParams) 
     },
     z.object(shape, params),
   );
+}
+
+// array
+export function array<T extends ZodTypeAny>(schema: T, params?: RawCreateParams) {
+  return z.preprocess(
+    val => {
+      // also undefined even val is 'null'
+      if (!val) return undefined;
+      if (val === 'undefined' || val === 'null') return undefined;
+      return JSON.parse(String(val));
+    },
+    z.array(schema, params),
+  );
+}
+
+// object
+export function object<T extends ZodRawShape>(shape: T, params?: RawCreateParams) {
+  return z.object(shape, params);
 }
 
 // infer
