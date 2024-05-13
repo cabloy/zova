c3de90d3347a3a937ffb7aa0e533dfdad58b9df8
zhennann <zhen.nann@icloud.com>
2024-05-13 10:09:25 +0800

    Update use.ts

--- cabloy-front-core/packages-cabloy/core/src/decorator/class/use.ts
+++ cabloy-front-core/packages-cabloy/core/src/decorator/class/use.ts
@@ -1,10 +1,16 @@
+import { IBeanRecord } from '../../bean/type.js';
 import { appMetadata, MetadataKey } from '../../core/metadata.js';
 import { appResource } from '../../core/resource.js';
 import { Constructable, IDecoratorUseOptions } from '../index.js';
 
-export function Use(options?: IDecoratorUseOptions): PropertyDecorator {
+export function Use<T extends keyof IBeanRecord>(beanFullName?: T): PropertyDecorator;
+export function Use(options?: IDecoratorUseOptions): PropertyDecorator;
+export function Use(options?: string | IDecoratorUseOptions): PropertyDecorator {
   return function (target: object, prop: MetadataKey) {
     if (!options) options = {};
+    if (typeof options === 'string') {
+      options = { beanFullName: options } as unknown as IDecoratorUseOptions;
+    }
     // beanClass
     const beanClass = appMetadata.getDesignType(target, prop) as Constructable;
     // record
