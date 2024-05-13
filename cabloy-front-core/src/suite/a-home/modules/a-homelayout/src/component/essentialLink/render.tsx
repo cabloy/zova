92d22e62da1db250cbcc431999aa86592f8e8988
zhennann <zhen.nann@icloud.com>
2024-05-14 07:35:00 +0800

    Update render.tsx

--- cabloy-front-core/src/suite/a-home/modules/a-homelayout/src/component/essentialLink/render.tsx
+++ cabloy-front-core/src/suite/a-home/modules/a-homelayout/src/component/essentialLink/render.tsx
@@ -2,23 +2,23 @@ import { BeanRenderBase, CabloyIcon, Local } from '@cabloy/front';
 import type { MotherEssentialLink } from './mother.js';
 import { RouterLink } from 'vue-router';
 
-export interface RenderEssentialLink extends MotherEssentialLink { }
+export interface RenderEssentialLink extends MotherEssentialLink {}
 
 @Local()
 export class RenderEssentialLink extends BeanRenderBase {
   _renderLink() {
     const domContent = [
       <CabloyIcon name={this.$props.icon} height={24} width={24}></CabloyIcon>,
-      <span>{this.$props.title}</span>
-    ]
+      <span>{this.$props.title}</span>,
+    ];
     if (this.$props.href) {
       return (
-        <a href={this.$props.href} target='_blank'>{domContent}</a>
-      )
+        <a href={this.$props.href} target="_blank">
+          {domContent}
+        </a>
+      );
     }
-    return (
-      <RouterLink to={this.$props.to!}>{domContent}</RouterLink>
-    )
+    return <RouterLink to={this.$props.to!}>{domContent}</RouterLink>;
   }
 
   render() {
