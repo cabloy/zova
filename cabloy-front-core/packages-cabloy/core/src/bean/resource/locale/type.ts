ab5fea07a06891616b982d9b443c9fc7929cf5ab
zhennann <zhen.nann@icloud.com>
2024-05-13 11:40:06 +0800

    feat: local info

--- cabloy-front-core/packages-cabloy/core/src/bean/resource/locale/type.ts
+++ cabloy-front-core/packages-cabloy/core/src/bean/resource/locale/type.ts
@@ -1,11 +1,11 @@
 export interface IModuleLocale {
   (...args: any[]): string;
-  locale: (locale: string, ...args: any[]) => string;
+  locale: <T extends keyof ILocalInfos>(locale: T, ...args: any[]) => string;
 }
 
 export interface IModuleLocaleText {
   (text: string, ...args: any[]): string;
-  locale: (locale: string, text: string, ...args: any[]) => string;
+  locale: <T extends keyof ILocalInfos>(locale: T, text: string, ...args: any[]) => string;
 }
 
 export type TypeModuleLocales<T> = {
@@ -13,3 +13,9 @@ export type TypeModuleLocales<T> = {
 };
 
 export type TypeLocaleBase = 'zh-cn';
+
+export interface ILocalInfo {}
+export interface ILocalInfos {
+  'en-us': ILocalInfo;
+  'zh-cn': ILocalInfo;
+}
--- cabloy-front-core/packages-cabloy/core/src/core/component/locale.ts
+++ cabloy-front-core/packages-cabloy/core/src/core/component/locale.ts
@@ -1,7 +1,7 @@
 import * as localeutil from '@cabloy/localeutil';
 import { BeanSimple } from '../../bean/beanSimple.js';
 import { TypeModuleResourceLocaleModules, TypeModuleResourceLocales } from '../../types/interface/module.js';
-import { IModuleLocale, IModuleLocaleText } from '../../bean/resource/locale/type.js';
+import { ILocalInfos, IModuleLocale, IModuleLocaleText } from '../../bean/resource/locale/type.js';
 import { CabloyLocaleOptionalMap } from '../app/locale.js';
 
 const SymbolLocale = Symbol('SymbolLocale');
@@ -52,7 +52,11 @@ export class AppLocale extends BeanSimple {
     const getText = function (text: string, ...args: any[]): string {
       return self.getText(moduleScope, undefined, text, ...args);
     };
-    getText.locale = function (locale: string | undefined, text: string, ...args: any[]): string {
+    getText.locale = function <T extends keyof ILocalInfos> (
+      locale: T | undefined,
+      text: string,
+      ...args: any[]
+    ): string {
       return self.getText(moduleScope, locale, text, ...args);
     };
     return getText;
@@ -64,13 +68,18 @@ export class AppLocale extends BeanSimple {
     const getText = function (...args: any[]): string {
       return self.getText(moduleScope, undefined, text, ...args);
     };
-    getText.locale = function (locale: string | undefined, ...args: any[]): string {
+    getText.locale = function <T extends keyof ILocalInfos> (locale: T | undefined, ...args: any[]): string {
       return self.getText(moduleScope, locale, text, ...args);
     };
     return getText;
   }
 
-  public getText(moduleScope: string | undefined, locale: string | undefined, key: string, ...args: any[]): string {
+  public getText<T extends keyof ILocalInfos>(
+    moduleScope: string | undefined,
+    locale: T | undefined,
+    key: string,
+    ...args: any[]
+  ): string {
     return localeutil.getLocaleText(
       moduleScope ? this.localeModules[moduleScope] : undefined,
       this.locales,
