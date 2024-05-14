288f232396a6f7b4d8b3797741396a95f2c8bd3a
zhennann <zhen.nann@icloud.com>
2024-05-14 10:41:17 +0800

    Create virtual.router.ts

---
+++ cabloy-front-core/src/suite-vendor/a-core/modules/a-router/src/bean/virtual.router.ts
@@ -0,0 +1,22 @@
+import { BeanBase, TypeEventOff, Virtual } from '@cabloy/front';
+import { StoreRouterLike } from './store.router.js';
+
+@Virtual()
+export class StoreRouter extends BeanBase {
+  private _eventRouterGuards: TypeEventOff;
+
+  protected async __init__() {
+    this._eventRouterGuards = this.app.meta.event.on('a-router:routerGuards', async (context, next) => {
+      this.onRouterGuards(context.data);
+      await next();
+    });
+  }
+
+  protected __dispose__() {
+    if (this._eventRouterGuards) {
+      this._eventRouterGuards();
+    }
+  }
+
+  protected onRouterGuards(_router: StoreRouterLike) {}
+}
