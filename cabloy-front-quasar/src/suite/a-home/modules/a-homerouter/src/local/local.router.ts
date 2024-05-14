b4fa30e3a1eae9c8c23d0bc925cee298a4fea459
zhennann <zhen.nann@icloud.com>
2024-05-14 11:20:53 +0800

    feat: router

---
+++ cabloy-front-quasar/src/suite/a-home/modules/a-homerouter/src/local/local.router.ts
@@ -0,0 +1,12 @@
+import { Local } from '@cabloy/front';
+import { ScopeModule } from '../resource/this.js';
+import { StoreRouterLike, VirtualRouter } from 'cabloy-module-front-a-router';
+
+@Local()
+export class LocalRouter extends VirtualRouter<ScopeModule> {
+  protected onRouterGuards(router: StoreRouterLike) {
+    router.beforeEach(async _to => {
+      //console.log(to);
+    });
+  }
+}
