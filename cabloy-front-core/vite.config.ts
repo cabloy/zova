c8e319de4f4f78d82bc844ad0a493e1ece05c4a2
zhennann <zhen.nann@icloud.com>
2024-05-15 17:33:15 +0800

    Update vite.config.ts

--- cabloy-front-core/vite.config.ts
+++ cabloy-front-core/vite.config.ts
@@ -3,17 +3,18 @@ import Vue from '@vitejs/plugin-vue';
 
 // Utilities
 import { defineConfig, mergeConfig } from 'vite';
-import { getFlavor } from '@cabloy/app-vite';
+import { getAppMode, getFlavor } from '@cabloy/app-vite';
 import { CabloyConfigMeta } from '@cabloy/front';
 import { generateCabloyViteMeta } from '@cabloy/app-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
   const flavor = getFlavor();
+  const appMode = getAppMode();
   const configMeta: CabloyConfigMeta = {
     flavor,
     mode,
-    appMode: 'spa',
+    appMode,
   };
   const configOptions = {
     appDir: process.cwd(),
