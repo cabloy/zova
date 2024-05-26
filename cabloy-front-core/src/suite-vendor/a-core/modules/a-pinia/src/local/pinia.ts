cb863615c09ea7da8c75e1e1a2eda1a0be5115d6
zhennann <zhen.nann@icloud.com>
2024-05-26 21:34:47 +0800

    Update pinia.ts

--- cabloy-front-core/src/suite-vendor/a-core/modules/a-pinia/src/local/pinia.ts
+++ cabloy-front-core/src/suite-vendor/a-core/modules/a-pinia/src/local/pinia.ts
@@ -1,9 +1,15 @@
 import { BeanBase, Local } from '@cabloy/front';
 import { ScopeModule } from '../resource/this.js';
+import { createPinia } from 'pinia';
 
 @Local()
 export class Pinia extends BeanBase<ScopeModule> {
-  protected async __init__() {}
+  pinia;
+
+  protected async __init__() {
+    this.pinia = createPinia();
+    this.app.vue.use(this.pinia);
+  }
 
   protected __dispose__() {}
 }
