ee2104a4c062d865eca2af58ac3d2a156bb53916
zhennann <zhen.nann@icloud.com>
2024-05-27 14:30:38 +0800

    feat: pinia store

---
+++ cabloy-front-core/src/suite-vendor/a-core/modules/a-pinia/src/bean/virtual.pinia.ts
@@ -0,0 +1,20 @@
+import { BeanBase, Virtual } from '@cabloy/front';
+
+const SymbolPiniaStore = Symbol('SymbolPiniaStore');
+
+@Virtual()
+export class VirtualPinia<TScopeModule = unknown> extends BeanBase<TScopeModule> {
+  [SymbolPiniaStore];
+
+  protected __get__(prop) {
+    return this[SymbolPiniaStore][prop];
+  }
+
+  protected __set__(prop, value) {
+    this[SymbolPiniaStore][prop] = value;
+  }
+
+  protected async __init__(useStore) {
+    this[SymbolPiniaStore] = useStore();
+  }
+}
--- cabloy-front-core/src/suite-vendor/a-core/modules/a-pinia/src/resource/beans.ts
+++ cabloy-front-core/src/suite-vendor/a-core/modules/a-pinia/src/resource/beans.ts
@@ -1,3 +1,4 @@
+export * from '../bean/virtual.pinia.js';
 declare module '@cabloy/front' {
   export interface IBeanRecord {}
 }
