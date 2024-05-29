96257c6bd5ea1fbf71b0af6093d0f29974c0b4ac
zhennann <zhen.nann@icloud.com>
2024-05-29 15:57:33 +0800

    feat: UseHook

--- cabloy-front-core/packages-cabloy/core/src/decorator/class/useHook.ts
+++ cabloy-front-core/packages-cabloy/core/src/decorator/class/useHook.ts
@@ -1,10 +1,12 @@
 import { MetadataKey } from '../../core/metadata.js';
 import { appResource } from '../../core/resource.js';
-import { IDecoratorUseHookOptions } from '../index.js';
+import { Functionable, IDecoratorUseHookOptions } from '../index.js';
 
-export function UseHook(options?: IDecoratorUseHookOptions): PropertyDecorator {
+export function UseHook(options: IDecoratorUseHookOptions | Functionable): PropertyDecorator {
   return function (target: object, prop: MetadataKey) {
-    if (!options) options = {};
+    if (typeof options === 'function') {
+      options = { beanHook: options } as IDecoratorUseHookOptions;
+    }
     // record
     appResource.addUse(target, {
       ...options,
