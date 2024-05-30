f650170182b664d54b86cbb42e71ddabca5e307b
zhennann <zhen.nann@icloud.com>
2024-05-30 08:02:16 +0800

    Update beanRenderBase.ts

--- cabloy-front-core/packages-cabloy/core/src/bean/beanRenderBase.ts
+++ cabloy-front-core/packages-cabloy/core/src/bean/beanRenderBase.ts
@@ -1,20 +1,22 @@
-import { Use } from '../decorator/class/use.js';
 import { Cast } from '../types/utils/cast.js';
 import { BeanBase } from './beanBase.js';
 
+const SymbolController = Symbol('SymbolController');
+
 export class BeanRenderBase<TScopeModule = unknown> extends BeanBase<TScopeModule> {
-  @Use()
-  $$controller: unknown;
+  private get [SymbolController]() {
+    return this.bean._getBeanSyncOnly('$$controller');
+  }
 
   render() {
     return;
   }
 
   protected __get__(prop) {
-    return Cast(this).$$controller[prop];
+    return Cast(this[SymbolController])[prop];
   }
 
   protected __set__(prop, value) {
-    Cast(this).$$controller[prop] = value;
+    Cast(this[SymbolController])[prop] = value;
   }
 }
