c8cd6fbfff71d73206152cda22e3111e84a3a939
zhennann <zhen.nann@icloud.com>
2024-05-31 09:53:11 +0800

    Update onControllerMounted.ts

--- cabloy-front-core/packages-cabloy/core/src/composables/onControllerMounted.ts
+++ cabloy-front-core/packages-cabloy/core/src/composables/onControllerMounted.ts
@@ -2,6 +2,9 @@ import { useContext } from './useContext.js';
 
 export async function onControllerMounted(fn) {
   const ctx = useContext();
+  if (!ctx) {
+    throw new Error('run in the invalid context');
+  }
   await ctx.meta.state.mounted.wait();
   if (fn) {
     return await ctx.meta.util.instanceScope(fn);
