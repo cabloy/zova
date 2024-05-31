44b1e0f04f87e9178e1d60b5bcdcdb710bdb520e
zhennann <zhen.nann@icloud.com>
2024-05-31 22:19:14 +0800

    Update typings.ts

--- cabloy-front-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/typings.ts
+++ cabloy-front-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/typings.ts
@@ -1,1 +1,79 @@
-export {};
+import 'quasar';
+declare module 'quasar' {
+  export interface QBtnToggleProps {
+    // @ts-ignore: ignore
+    modelValue?: any;
+  }
+  export interface QCheckboxProps {
+    // @ts-ignore: ignore
+    modelValue?: any | any[];
+  }
+  export interface QColorProps {
+    // @ts-ignore: ignore
+    modelValue?: string | null | undefined;
+  }
+  export interface QDateProps {
+    // @ts-ignore: ignore
+    modelValue?: string | any[] | any | null | undefined;
+  }
+  export interface QEditorProps {
+    // @ts-ignore: ignore
+    modelValue?: string;
+  }
+  export interface QFieldSlots {
+    // @ts-ignore: ignore
+    modelValue?: any;
+  }
+  export interface QFileProps {
+    // @ts-ignore: ignore
+    modelValue?: File | FileList | any[] | null | undefined;
+  }
+  export interface QInputProps {
+    // @ts-ignore: ignore
+    modelValue?: string | number | null | undefined;
+  }
+  export interface QKnobProps {
+    // @ts-ignore: ignore
+    modelValue?: number;
+  }
+  export interface QOptionGroupProps {
+    // @ts-ignore: ignore
+    modelValue?: any;
+  }
+  export interface QPaginationProps {
+    // @ts-ignore: ignore
+    modelValue?: number;
+  }
+  export interface QPopupEditProps {
+    // @ts-ignore: ignore
+    modelValue?: any;
+  }
+  export interface QRadioProps {
+    // @ts-ignore: ignore
+    modelValue?: number | string | null | undefined;
+  }
+  export interface QRatingProps {
+    // @ts-ignore: ignore
+    modelValue?: number;
+  }
+  export interface QSelectProps {
+    // @ts-ignore: ignore
+    modelValue?: any;
+  }
+  export interface QSliderProps {
+    // @ts-ignore: ignore
+    modelValue?: number | null | undefined;
+  }
+  export interface QSplitterProps {
+    // @ts-ignore: ignore
+    modelValue?: number;
+  }
+  export interface QTimeProps {
+    // @ts-ignore: ignore
+    modelValue?: string | null | undefined;
+  }
+  export interface QToggleProps {
+    // @ts-ignore: ignore
+    modelValue?: any | any[];
+  }
+}
