5d8c87cd3b9b1ee32cb921be7068b2fabc3a0229
zhennann <zhen.nann@icloud.com>
2024-06-01 17:45:25 +0800

    Update zova.ts

--- zova-dev/packages-zova/core/src/plugins/zova.ts
+++ zova-dev/packages-zova/core/src/plugins/zova.ts
@@ -1,10 +1,10 @@
 import { App } from 'vue';
 import { ZovaApplication } from '../core/index.js';
-import { PluginZovaOptions } from '../types/interface/pluginCabloy.js';
+import { PluginZovaOptions } from '../types/interface/pluginZova.js';
 
 export const PluginZova = {
   async install(vue: App, { modulesMeta, AppMonkey, locales, config }: PluginZovaOptions) {
-    // cabloy app
+    // zova app
     const app = new ZovaApplication(vue);
     await app.initialize({ modulesMeta, AppMonkey, locales, config });
   },
