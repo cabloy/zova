ac0f3086ee3b8ece08ecc53c27d10a7f1297021b
zhennann <zhen.nann@icloud.com>
2024-06-05 10:51:13 +0800

    Update extendViteConf.ts

--- zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendViteConf.ts
+++ zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendViteConf.ts
@@ -5,6 +5,7 @@ import { ConfigContext } from './types.js';
 export function extendViteConf(context: ConfigContext) {
   return function extendViteConf(conf) {
     const zovaViteMeta = context.zovaViteMeta as ZovaViteConfigResult;
+    // conf.build override zovaViteMeta.viteConfig.build
     conf.build = mergeConfig({}, zovaViteMeta.viteConfig.build, conf.build as unknown as any);
   };
 }
