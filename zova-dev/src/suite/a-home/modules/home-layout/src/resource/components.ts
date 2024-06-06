788d52cd6522385c0ee40b1c351e08423a069256
zhennann <zhen.nann@icloud.com>
2024-06-06 12:06:32 +0800

    Update components.ts

--- zova-dev/src/suite/a-home/modules/home-layout/src/resource/components.ts
+++ zova-dev/src/suite/a-home/modules/home-layout/src/resource/components.ts
@@ -5,3 +5,21 @@ export const components = {
   layoutDefault,
   layoutEmpty,
 };
+
+export * as NSControllerLayoutDefault from '../component/layoutDefault/controller.js';
+import * as NSControllerLayoutDefault from '../component/layoutDefault/controller.js';
+export * as NSControllerLayoutEmpty from '../component/layoutEmpty/controller.js';
+import * as NSControllerLayoutEmpty from '../component/layoutEmpty/controller.js';
+
+import 'zova';
+declare module 'zova' {
+  export interface IComponentRecord {
+    'home-layout:layoutDefault': typeof NSControllerLayoutDefault;
+    'home-layout:layoutEmpty': typeof NSControllerLayoutEmpty;
+  }
+
+  export interface IComponentLayoutRecord {
+    'home-layout:layoutDefault': typeof NSControllerLayoutDefault;
+    'home-layout:layoutEmpty': typeof NSControllerLayoutEmpty;
+  }
+}
