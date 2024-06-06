9f14845f18dd40b2eae29cdc1d07d606de1da2c4
zhennann <zhen.nann@icloud.com>
2024-06-06 15:51:42 +0800

    feat: __moduleBelong__

--- zova-dev/packages-zova/core/src/bean/beanBaseSimple.ts
+++ zova-dev/packages-zova/core/src/bean/beanBaseSimple.ts
@@ -3,10 +3,11 @@ import { StateLock } from '../utils/stateLock.js';
 import { BeanSimple } from './beanSimple.js';
 
 export const SymbolBeanFullName = Symbol('SymbolBeanFullName');
+export const SymbolModuleBelong = Symbol('SymbolModuleBelong');
 
 export class BeanBaseSimple extends BeanSimple {
   private [SymbolBeanFullName]: string;
-  private __moduleBelong__?: string;
+  private [SymbolModuleBelong]?: string;
   // @ts-ignore: ignore
   private __inited__: StateLock;
 
@@ -15,11 +16,11 @@ export class BeanBaseSimple extends BeanSimple {
     if (moduleBelong && typeof moduleBelong !== 'string') {
       throw new Error(`moduleBelong not valid: ${moduleBelong}`);
     }
-    this.__moduleBelong__ = moduleBelong;
+    this[SymbolModuleBelong] = moduleBelong;
     this.__inited__ = StateLock.create();
   }
 
   protected get moduleBelong() {
-    return this.__moduleBelong__ || appResource._getModuleBelong(this[SymbolBeanFullName]);
+    return this[SymbolModuleBelong] || appResource._getModuleBelong(this[SymbolBeanFullName]);
   }
 }
