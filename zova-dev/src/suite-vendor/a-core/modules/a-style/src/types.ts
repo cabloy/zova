4a246c890992460646d2682dcd1c0902c8c6ce6f
zhennann <zhen.nann@icloud.com>
2024-06-09 22:16:16 +0800

    feat: $style

--- zova-dev/src/suite-vendor/a-core/modules/a-style/src/types.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-style/src/types.ts
@@ -1,1 +1,7 @@
-export {};
+import { style } from 'typestyle';
+import 'zova';
+declare module 'zova' {
+  export interface BeanBase {
+    $style: typeof style;
+  }
+}
--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/style/style.ts
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/style/style.ts
@@ -1,4 +1,3 @@
-import { style } from 'typestyle';
 import { BeanStyleBase, Local, useComputed } from 'zova';
 import { ScopeModule } from '../../resource/this.js';
 import { ControllerPageStyle } from './controller.js';
@@ -10,7 +9,7 @@ export class StyleStyle extends BeanStyleBase<ScopeModule> {
   textColor;
   protected async __init__() {
     this.textColor = useComputed(() => {
-      return style({ $debugName: 'a-demo', color: this.active ? 'orange' : '' });
+      return this.$style({ $debugName: 'a-demo', color: this.active ? 'orange' : '' });
     });
   }
 }
