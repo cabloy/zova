92b2e2ee2e37fae8fb1f7652f9a6591047809534
zhennann <zhen.nann@icloud.com>
2024-06-09 21:35:28 +0800

    feat: style

---
+++ zova-dev/src/suite-vendor/a-core/modules/a-style/src/resource/scope.ts
@@ -0,0 +1,30 @@
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'zova';
+import { config, Errors, locales, constants } from '../config/index.js';
+import { components } from './components.js';
+
+@Scope()
+export class ScopeModuleAStyle extends BeanScopeBase {}
+
+export interface ScopeModuleAStyle
+  extends TypeModuleResource<
+    typeof components,
+    typeof config,
+    typeof Errors,
+    (typeof locales)[TypeLocaleBase],
+    typeof constants
+  > {}
+
+import 'zova';
+declare module 'zova' {
+  export interface IBeanScopeRecord {
+    'a-style': ScopeModuleAStyle;
+  }
+
+  export interface IBeanScopeConfig {
+    'a-style': ReturnType<typeof config>;
+  }
+
+  export interface IBeanScopeLocale {
+    'a-style': (typeof locales)[TypeLocaleBase];
+  }
+}
