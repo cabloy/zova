0b24518487c2e8e9864fe6fe9fa97122c98eb4e3
zhennann <zhen.nann@icloud.com>
2024-06-10 09:00:06 +0800

    Update style.ts

--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/style/style.ts
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/style/style.ts
@@ -6,7 +6,7 @@ export interface StyleStyle extends ControllerPageStyle {}
 
 @Local()
 export class StyleStyle extends BeanStyleBase<ScopeModule> {
-  textColor;
+  textColor: string;
   protected async __init__() {
     this.textColor = useComputed(() => {
       return this.$style({ color: this.active ? 'orange' : '' });
