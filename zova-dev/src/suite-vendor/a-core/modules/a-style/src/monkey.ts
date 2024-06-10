73c570997b5d2e3a025ac68f379067bace4eaeb1
zhennann <zhen.nann@icloud.com>
2024-06-10 22:15:27 +0800

    Update monkey.ts

--- zova-dev/src/suite-vendor/a-core/modules/a-style/src/monkey.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-style/src/monkey.ts
@@ -1,17 +1,15 @@
 import { style } from 'typestyle';
-import { BeanBase, BeanContainerLike, BeanSimple, IModule, IMonkeyModule, IMonkeySystem, SymbolModuleName } from 'zova';
-import { ScopeModule } from './resource/this.js';
+import { BeanBase, BeanContainerLike, BeanSimple, IMonkeySystem, SymbolModuleName } from 'zova';
+import { ScopeModule, __ThisModule__ } from './resource/this.js';
 
-export class Monkey extends BeanSimple implements IMonkeySystem, IMonkeyModule {
+export class Monkey extends BeanSimple implements IMonkeySystem {
   private _storeStyleDefault: any;
-  private _moduleSelf: IModule;
 
-  constructor(moduleSelf: IModule) {
-    super();
-    this._moduleSelf = moduleSelf;
+  async appInitialize() {
+    const scope: ScopeModule = await this.bean.getScope(__ThisModule__);
+    this._storeStyleDefault = await this.bean._newBean(scope.config.defaultStyle, true);
+    console.log(this._storeStyleDefault);
   }
-
-  async appInitialize() {}
   async appInitialized() {}
   async beanInit(bean: BeanContainerLike, beanInstance: BeanBase) {
     const self = this;
@@ -28,14 +26,6 @@ export class Monkey extends BeanSimple implements IMonkeySystem, IMonkeyModule {
   async beanInited(_bean: BeanContainerLike, _beanInstance: BeanBase) {}
   beanDispose(_bean: BeanContainerLike, _beanInstance: BeanBase) {}
   beanDisposed(_bean: BeanContainerLike, _beanInstance: BeanBase) {}
-  async moduleLoading(_module: IModule) {}
-  async moduleLoaded(module: IModule) {
-    if (this._moduleSelf !== module) return;
-    const scope: ScopeModule = await this.bean.getScope(this._moduleSelf.info.relativeName);
-    this._storeStyleDefault = await this.bean._newBean(scope.config.defaultStyle, true);
-    console.log(this._storeStyleDefault);
-  }
-  async configLoaded(_module: IModule, _config) {}
 
   _patchStyle(beanInstance: BeanBase, props, ...args) {
     if (props && typeof props === 'object') {
