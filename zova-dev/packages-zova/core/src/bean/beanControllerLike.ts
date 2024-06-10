f903d840ae511cd9c446acbcb3fc3f6316960809
zhennann <zhen.nann@icloud.com>
2024-06-10 11:54:15 +0800

    Update beanControllerLike.ts

--- zova-dev/packages-zova/core/src/bean/beanControllerLike.ts
+++ zova-dev/packages-zova/core/src/bean/beanControllerLike.ts
@@ -5,19 +5,20 @@ import { BeanControllerIdentifier } from './type.js';
 const SymbolController = Symbol('SymbolController');
 
 export class BeanControllerLike<TScopeModule = unknown> extends BeanBase<TScopeModule> {
-  protected get [SymbolController](): unknown {
+  protected get [SymbolController](): unknown | undefined {
     return this.bean._getBeanSyncOnly(BeanControllerIdentifier);
   }
 
   /** @internal */
   public __get__(prop): unknown {
     const controller = Cast(this[SymbolController]);
-    return controller[prop];
+    return controller?.[prop];
   }
 
   /** @internal */
   public __set__(prop, value): boolean {
     const controller = Cast(this[SymbolController]);
+    if (!controller) return false;
     if (prop in controller) {
       controller[prop] = value;
       return true;
