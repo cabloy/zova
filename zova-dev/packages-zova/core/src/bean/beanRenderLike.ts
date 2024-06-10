74179a80c124852097553f65780628f7fcd3bc7a
zhennann <zhen.nann@icloud.com>
2024-06-10 11:58:06 +0800

    Update beanRenderLike.ts

--- zova-dev/packages-zova/core/src/bean/beanRenderLike.ts
+++ zova-dev/packages-zova/core/src/bean/beanRenderLike.ts
@@ -5,7 +5,7 @@ import { BeanStyleIdentifier } from './type.js';
 const SymbolStyle = Symbol('SymbolStyle');
 
 export class BeanRenderLike<TScopeModule = unknown> extends BeanControllerLike<TScopeModule> {
-  private get [SymbolStyle](): unknown {
+  private get [SymbolStyle](): unknown | undefined {
     return this.bean._getBeanSyncOnly(BeanStyleIdentifier);
   }
 
@@ -14,7 +14,7 @@ export class BeanRenderLike<TScopeModule = unknown> extends BeanControllerLike<T
     const value = super.__get__(prop);
     if (value !== undefined) return value;
     const style = Cast(this[SymbolStyle]);
-    return style[prop];
+    return style?.[prop];
   }
 
   /** @internal */
@@ -22,6 +22,7 @@ export class BeanRenderLike<TScopeModule = unknown> extends BeanControllerLike<T
     const res = super.__set__(prop, value);
     if (res) return res;
     const style = Cast(this[SymbolStyle]);
+    if (!style) return false;
     if (prop in style) {
       style[prop] = value;
       return true;
