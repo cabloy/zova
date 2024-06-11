b70b455e1da44a40905c260d6bfbea2c03b963be
zhennann <zhen.nann@icloud.com>
2024-06-11 10:25:21 +0800

    feat: devui

---
+++ zova-dev/src/suite-vendor/a-devui/modules/a-devui/src/resource/scope.ts
@@ -0,0 +1,30 @@
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'zova';
+import { config, Errors, locales, constants } from '../config/index.js';
+import { components } from './components.js';
+
+@Scope()
+export class ScopeModuleADevui extends BeanScopeBase {}
+
+export interface ScopeModuleADevui
+  extends TypeModuleResource<
+    typeof components,
+    typeof config,
+    typeof Errors,
+    (typeof locales)[TypeLocaleBase],
+    typeof constants
+  > {}
+
+import 'zova';
+declare module 'zova' {
+  export interface IBeanScopeRecord {
+    'a-devui': ScopeModuleADevui;
+  }
+
+  export interface IBeanScopeConfig {
+    'a-devui': ReturnType<typeof config>;
+  }
+
+  export interface IBeanScopeLocale {
+    'a-devui': (typeof locales)[TypeLocaleBase];
+  }
+}
---
+++ zova-dev/src/suite-vendor/a-devui/modules/a-devui/src/resource/this.ts
@@ -0,0 +1,4 @@
+export const __ThisModule__ = 'a-devui';
+export type __ThisModuleType__ = typeof __ThisModule__;
+
+export { ScopeModuleADevui as ScopeModule } from './scope.js';
