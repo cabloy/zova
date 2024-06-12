2c1cfeae978a4708a7d39d7672ac229f6d8dff74
zhennann <zhen.nann@icloud.com>
2024-06-12 15:27:33 +0800

    feat: themeToken

---
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/themeToken.ts
@@ -0,0 +1,13 @@
+export interface ThemeToken {
+  color: {
+    primary: string;
+    secondary: string;
+    accent: string;
+    dark: string;
+    darkPage: string;
+    positive: string;
+    negative: string;
+    info: string;
+    warning: string;
+  };
+}
--- zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/types.ts
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/types.ts
@@ -1,9 +1,18 @@
 import { ReturnTypeComposable } from 'zova';
 import useQuasar from 'quasar/src/composables/use-quasar/use-quasar.js';
+import { ThemeToken } from './themeToken.js';
 
 import 'zova';
 declare module 'zova' {
   export interface BeanBase {
     $q: ReturnTypeComposable<typeof useQuasar>;
+    $token: ThemeToken;
+  }
+}
+
+import 'zova-module-a-style';
+declare module 'zova-module-a-style' {
+  export interface ThemeApplyResult {
+    token: ThemeToken;
   }
 }
