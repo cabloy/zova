4cda9594664b405a5e89ce4fc078401f10180fd8
zhennann <zhen.nann@icloud.com>
2024-06-12 11:18:01 +0800

    Update theme.orange.ts

--- zova-dev/src/suite/a-demo/modules/a-demo/src/bean/theme.orange.ts
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/bean/theme.orange.ts
@@ -1,9 +1,25 @@
 import { BeanBase, Theme } from 'zova';
 import { ScopeModule } from '../resource/this.js';
+import { ThemeApplyParams, ThemeApplyResult, ThemeBase } from 'zova-module-a-style';
+import { ThemeToken } from 'zova-module-a-devui';
 
 @Theme()
-export class ThemeOrange extends BeanBase<ScopeModule> {
-  protected async __init__() {}
-
-  protected __dispose__() {}
+export class ThemeOrange extends BeanBase<ScopeModule> implements ThemeBase {
+  async apply({ name: _name, dark }: ThemeApplyParams): Promise<ThemeApplyResult> {
+    const token: ThemeToken = {
+      color: {
+        primary: '#f28238',
+      },
+      var: {
+        borderColor: '#f28d49',
+      },
+      component: {
+        page: {
+          background: dark ? '#121212' : '#fff',
+          color: dark ? '#fff' : '#000',
+        },
+      },
+    };
+    return { token };
+  }
 }
