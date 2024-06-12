6a5d73303ae9045ce903b7ad889b99c762dfe4e5
zhennann <zhen.nann@icloud.com>
2024-06-12 20:55:45 +0800

    feat: demo style

---
+++ zova-ui-antdv/src/suite/a-demo/modules/a-demo/src/page/style/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <template></template>
+</template>
+
+<script setup lang="ts">
+import { useControllerPage } from 'zova';
+import { ControllerPageStyle } from './controller.js';
+import { RenderStyle } from './render.jsx';
+import { StyleStyle } from './style.js';
+useControllerPage(ControllerPageStyle, RenderStyle, StyleStyle);
+</script>
