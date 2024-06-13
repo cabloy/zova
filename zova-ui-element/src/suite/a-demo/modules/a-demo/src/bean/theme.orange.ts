371e7d063a0c2a38a1e44b56d65d57db80afb646
zhennann <zhen.nann@icloud.com>
2024-06-13 16:24:26 +0800

    feat: theme orange

---
+++ zova-ui-element/src/suite/a-demo/modules/a-demo/src/bean/theme.orange.ts
@@ -0,0 +1,26 @@
+import { BeanBase, Theme } from 'zova';
+import { ScopeModule } from '../resource/this.js';
+import { ThemeApplyParams, ThemeApplyResult, ThemeBase } from 'zova-module-a-style';
+import { ThemeToken } from 'zova-module-a-element';
+import { themeDark } from './themeDark.js';
+import { themeLight } from './themeLight.js';
+
+@Theme()
+export class ThemeOrange extends BeanBase<ScopeModule> implements ThemeBase {
+  apply({ name: _name, dark }: ThemeApplyParams): Promise<ThemeApplyResult> {
+    const token: ThemeToken = dark ? themeDark : themeLight;
+    const body = window?.document?.documentElement;
+    if (body) {
+      for (const key in token) {
+        const key2 = `--el-${key}`;
+        body.style.setProperty(key2, token[key]);
+      }
+      if (dark) {
+        body.classList.add('dark');
+      } else {
+        body.classList.remove('dark');
+      }
+    }
+    return { token };
+  }
+}
