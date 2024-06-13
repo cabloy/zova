bdb3bea6860bd85bbab4d57d93bf2cc0067f4035
zhennann <zhen.nann@icloud.com>
2024-06-13 12:09:08 +0800

    feat: theme light

--- zova-ui-element/src/suite/a-home/modules/home-theme/src/bean/theme.default.ts
+++ zova-ui-element/src/suite/a-home/modules/home-theme/src/bean/theme.default.ts
@@ -3,11 +3,12 @@ import { ScopeModule } from '../resource/this.js';
 import { ThemeApplyParams, ThemeApplyResult, ThemeBase } from 'zova-module-a-style';
 import { ThemeToken } from 'zova-module-a-element';
 import { themeDark } from './themeDark.js';
+import { themeLight } from './themeLight.js';
 
 @Theme()
 export class ThemeDefault extends BeanBase<ScopeModule> implements ThemeBase {
   async apply({ name: _name, dark }: ThemeApplyParams): Promise<ThemeApplyResult> {
-    const token: ThemeToken = dark ? themeDark : themeDark;
+    const token: ThemeToken = dark ? themeDark : themeLight;
     const body = window?.document?.documentElement;
     if (body) {
       for (const key in token) {
