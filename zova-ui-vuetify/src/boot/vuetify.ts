676e41b0c36273e167b01f1a7197f20872b4da1e
zhennann <zhen.nann@icloud.com>
2024-06-13 17:58:32 +0800

    feat: style & theme

--- zova-ui-vuetify/src/boot/vuetify.ts
+++ zova-ui-vuetify/src/boot/vuetify.ts
@@ -9,10 +9,18 @@ import 'vuetify/styles';
 
 // Composables
 import { createVuetify } from 'vuetify';
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
   },
 });
--- zova-ui-vuetify/src/suite/a-demo/modules/a-demo/src/page/style/render.tsx
+++ zova-ui-vuetify/src/suite/a-demo/modules/a-demo/src/page/style/render.tsx
@@ -1,7 +1,7 @@
-import { BeanRenderBase, Local } from 'zova';
+import { BeanRenderBase, Local, getBeanName } from 'zova';
 import type { StyleStyle } from './style.js';
 import { ScopeModule } from '../../resource/this.js';
-import { VBtn } from 'vuetify/components';
+import { VBtn, VIcon, VRadio, VRadioGroup } from 'vuetify/components';
 
 export interface RenderStyle extends StyleStyle {}
 
@@ -18,6 +18,22 @@ export class RenderStyle extends BeanRenderBase<ScopeModule> {
         >
           Switch Active
         </VBtn>
+        <hr></hr>
+        <div>
+          <VRadioGroup v-model={this.$theme.darkMode} inline>
+            <VRadio label="Light" value={false}></VRadio>
+            <VRadio label="Dark" value={true}></VRadio>
+            <VRadio label="Auto" value={'auto'}></VRadio>
+          </VRadioGroup>
+        </div>
+        <hr></hr>
+        <div>
+          <div style={{ color: this.$token.colors.primary }}>theme: {this.$theme.name}</div>
+          <VRadioGroup v-model={this.$theme.name} inline>
+            <VRadio label="Default" value={getBeanName('home-theme.theme.default')}></VRadio>
+            <VRadio label="Orange" value={getBeanName('a-demo.theme.orange')}></VRadio>
+          </VRadioGroup>
+        </div>
       </div>
     );
   }
