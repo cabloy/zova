968f1a8e2630e378f23fe9956c97d7fc7331e2ba
zhennann <zhen.nann@icloud.com>
2024-06-14 12:12:31 +0800

    feat: empty ui

--- zova-ui-empty/src/suite/a-home/modules/home-style/src/bean/style.default.ts
+++ zova-ui-empty/src/suite/a-home/modules/home-style/src/bean/style.default.ts
@@ -1,19 +1,12 @@
-import { BeanBase, Style, useComputed } from 'zova';
+import { BeanBase, Style } from 'zova';
 import { ScopeModule } from '../resource/this.js';
 
 @Style()
 export class StyleDefault extends BeanBase<ScopeModule> {
   textCenter: string;
-  buttonPrimary: string;
 
   protected async __init__() {
     this.textCenter = this.$style({ textAlign: 'center' });
-    this.buttonPrimary = useComputed(() => {
-      return this.$style({
-        color: this.$token.color.primary,
-        borderColor: this.$token.var.borderColor,
-      });
-    });
   }
 
   protected __dispose__() {}
