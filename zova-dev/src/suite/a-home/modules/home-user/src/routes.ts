53372f2eea3719650dd53e5157e4574c4f29ab92
zhennann <zhen.nann@icloud.com>
2024-06-18 16:48:56 +0800

    feat: route login

--- zova-dev/src/suite/a-home/modules/home-user/src/routes.ts
+++ zova-dev/src/suite/a-home/modules/home-user/src/routes.ts
@@ -1,4 +1,14 @@
 import Login from './page/login/index.vue';
 import { IModuleRoute } from 'zova-module-a-router';
 
-export const routes: IModuleRoute[] = [{ path: 'login', component: Login }];
+export const routes: IModuleRoute[] = [
+  //
+  {
+    path: 'login',
+    component: Login,
+    meta: {
+      layout: 'empty',
+      requiresAuth: false,
+    },
+  },
+];
