f3cc1a8932d53efc2185b345669e33f4555a3840
zhennann <zhen.nann@icloud.com>
2024-06-18 16:11:29 +0800

    feat: demo-user

---
+++ zova-dev/src/suite/a-demo/modules/demo-user/src/resource/scope.ts
@@ -0,0 +1,30 @@
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'zova';
+import { config, Errors, locales, constants } from '../config/index.js';
+import { components } from './components.js';
+
+@Scope()
+export class ScopeModuleDemoUser extends BeanScopeBase {}
+
+export interface ScopeModuleDemoUser
+  extends TypeModuleResource<
+    typeof components,
+    typeof config,
+    typeof Errors,
+    (typeof locales)[TypeLocaleBase],
+    typeof constants
+  > {}
+
+import 'zova';
+declare module 'zova' {
+  export interface IBeanScopeRecord {
+    'demo-user': ScopeModuleDemoUser;
+  }
+
+  export interface IBeanScopeConfig {
+    'demo-user': ReturnType<typeof config>;
+  }
+
+  export interface IBeanScopeLocale {
+    'demo-user': (typeof locales)[TypeLocaleBase];
+  }
+}
---
+++ zova-dev/src/suite/a-demo/modules/demo-user/src/resource/this.ts
@@ -0,0 +1,4 @@
+export const __ThisModule__ = 'demo-user';
+export type __ThisModuleType__ = typeof __ThisModule__;
+
+export { ScopeModuleDemoUser as ScopeModule } from './scope.js';
