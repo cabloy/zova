1b9c3d306653ccb2924ea0c8ee210f0099235f24
zhennann <zhen.nann@icloud.com>
2024-06-18 16:45:19 +0800

    feat: login

--- zova-dev/src/suite/a-home/modules/home-router/src/local/router.ts
+++ zova-dev/src/suite/a-home/modules/home-router/src/local/router.ts
@@ -1,12 +1,18 @@
-import { Local } from 'zova';
+import { Local, Use } from 'zova';
 import { ScopeModule } from '../resource/this.js';
 import { StoreRouterLike, BeanRouterBase } from 'zova-module-a-router';
+import { StoreUserInfo } from 'zova-module-home-user';
 
 @Local()
 export class Router extends BeanRouterBase<ScopeModule> {
+  @Use('home-user.store.userInfo')
+  $$userInfo: StoreUserInfo;
+
   protected onRouterGuards(router: StoreRouterLike) {
-    router.beforeEach(async _to => {
-      // console.log(to);
+    router.beforeEach(async to => {
+      if (to.meta.requiresAuth !== false && !this.$$userInfo.jwt) {
+        return '/home/user/login';
+      }
     });
   }
 }
