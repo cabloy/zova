f02f33350a137b1d027d2adacb912396be9bd529
zhennann <zhen.nann@icloud.com>
2024-06-19 18:22:53 +0800

    Update composable.ts

--- zova-dev/packages-zova/core/src/vue/composable.ts
+++ zova-dev/packages-zova/core/src/vue/composable.ts
@@ -1,17 +1,7 @@
-import { ComputedRef, Ref } from 'vue';
+import { UnwrapNestedRefs } from 'vue';
 import { Functionable } from '../decorator/type/functionable.js';
 
-export type ExtractComposable<Composable> = {
-  [Prop in keyof Composable]: Composable[Prop] extends Ref<infer RawType>
-    ? RawType
-    : Composable[Prop] extends ComputedRef<infer RawType>
-      ? RawType
-      : Composable[Prop] extends Functionable
-        ? Composable[Prop]
-        : Composable[Prop] extends object
-          ? ExtractComposable<Composable[Prop]>
-          : Composable[Prop];
-};
+export type ExtractComposable<Composable> = UnwrapNestedRefs<Composable>;
 
 export type ReturnTypeComposable<Composable extends Functionable> = Composable extends (...args: any[]) => infer R
   ? ExtractComposable<R>
