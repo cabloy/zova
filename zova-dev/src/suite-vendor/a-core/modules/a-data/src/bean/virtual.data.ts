9bc0492c0023eecc149d836e13b607b5096aa0fa
zhennann <zhen.nann@icloud.com>
2024-06-20 23:00:15 +0800

    feat: setQueryData

--- zova-dev/src/suite-vendor/a-core/modules/a-data/src/bean/virtual.data.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-data/src/bean/virtual.data.ts
@@ -1,3 +1,4 @@
+//import { persistQueryClientSave } from '@tanstack/query-persist-client-core';
 import {
   DefaultError,
   QueryKey,
@@ -48,6 +49,24 @@ export class BeanDataBase<TScopeModule = unknown> extends BeanBase<TScopeModule>
     params.queryKey = this._forcePrefix(params.queryKey);
     return this.ctx.meta.util.instanceScope(() => {
       const data = useQuery(params);
+      window.setTimeout(() => {
+        this.$queryClient.setQueryData(params.queryKey, 500);
+        const query = this.$queryClient.getQueryCache().find({ queryKey: params.queryKey })!;
+        const prefix = `${this.app.config.env.appName}-query`;
+        const storageKey = `${prefix}-${query.queryHash}`;
+        // Persist if we have storage defined, we use timeout to get proper state to be persisted
+        setTimeout(async () => {
+          localStorage.setItem(
+            storageKey,
+            await JSON.stringify({
+              state: query.state,
+              queryKey: query.queryKey,
+              queryHash: query.queryHash,
+              buster: '',
+            }),
+          );
+        }, 0);
+      }, 2000);
       return data;
     });
   }
--- zova-dev/src/suite/a-demo/modules/demo-todo/src/bean/data.todo.ts
+++ zova-dev/src/suite/a-demo/modules/demo-todo/src/bean/data.todo.ts
@@ -1,18 +1,28 @@
 import { Data } from 'zova';
 import { ScopeModule } from '../resource/this.js';
 import { BeanDataBase } from 'zova-module-a-data';
+import { experimental_createPersister } from '@tanstack/query-persist-client-core';
 
 @Data()
 export class DataTodo extends BeanDataBase<ScopeModule> {
   protected async __init__() {}
 
   select() {
+    const persister = experimental_createPersister({
+      storage: localStorage,
+      maxAge: 1000 * 60 * 60 * 12, // 12 hours
+      prefix: `${this.app.config.env.appName}-query`,
+      serialize: data => {
+        return JSON.stringify(data);
+      },
+    });
     return this.$useQuery({
       queryKey: ['select'],
       queryFn: async () => {
         await this.app.meta.util.sleep(1000);
         return 100;
       },
+      persister,
     });
   }
 }
