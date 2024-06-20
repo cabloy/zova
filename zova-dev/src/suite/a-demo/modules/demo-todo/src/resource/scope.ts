0609e1da35c751c61c6cdfcd096ce71a4171082d
zhennann <zhen.nann@icloud.com>
2024-06-20 09:39:53 +0800

    feat: demo-todo

---
+++ zova-dev/src/suite/a-demo/modules/demo-todo/src/resource/scope.ts
@@ -0,0 +1,30 @@
+import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'zova';
+import { config, Errors, locales, constants } from '../config/index.js';
+import { components } from './components.js';
+
+@Scope()
+export class ScopeModuleDemoTodo extends BeanScopeBase {}
+
+export interface ScopeModuleDemoTodo
+  extends TypeModuleResource<
+    typeof components,
+    typeof config,
+    typeof Errors,
+    (typeof locales)[TypeLocaleBase],
+    typeof constants
+  > {}
+
+import 'zova';
+declare module 'zova' {
+  export interface IBeanScopeRecord {
+    'demo-todo': ScopeModuleDemoTodo;
+  }
+
+  export interface IBeanScopeConfig {
+    'demo-todo': ReturnType<typeof config>;
+  }
+
+  export interface IBeanScopeLocale {
+    'demo-todo': (typeof locales)[TypeLocaleBase];
+  }
+}
---
+++ zova-dev/src/suite/a-demo/modules/demo-todo/src/resource/this.ts
@@ -0,0 +1,4 @@
+export const __ThisModule__ = 'demo-todo';
+export type __ThisModuleType__ = typeof __ThisModule__;
+
+export { ScopeModuleDemoTodo as ScopeModule } from './scope.js';
