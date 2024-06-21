5d0b87aa6fe1bf4c750ea4b85f7be85ad755f086
zhennann <zhen.nann@icloud.com>
2024-06-21 11:51:52 +0800

    feat: persisterSave

--- zova-dev/src/suite-vendor/a-core/modules/a-data/src/bean/virtual.data.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-data/src/bean/virtual.data.ts
@@ -1,4 +1,3 @@
-//import { persistQueryClientSave } from '@tanstack/query-persist-client-core';
 import {
   DefaultError,
   QueryKey,
@@ -7,28 +6,12 @@ import {
   UseQueryReturnType,
   UseQueryDefinedReturnType,
   UseQueryOptions,
+  QueryFilters,
+  Query,
 } from '@tanstack/vue-query';
 import { UnwrapNestedRefs } from 'vue';
-import { BeanBase, SymbolBeanFullName, Virtual } from 'zova';
-
-type NonUndefinedGuard<T> = T extends undefined ? never : T;
-
-type UndefinedInitialQueryOptions<
-  TQueryFnData = unknown,
-  TError = DefaultError,
-  TData = TQueryFnData,
-  TQueryKey extends QueryKey = QueryKey,
-> = UseQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey> & {
-  initialData?: undefined;
-};
-type DefinedInitialQueryOptions<
-  TQueryFnData = unknown,
-  TError = DefaultError,
-  TData = TQueryFnData,
-  TQueryKey extends QueryKey = QueryKey,
-> = UseQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey> & {
-  initialData: NonUndefinedGuard<TQueryFnData> | (() => NonUndefinedGuard<TQueryFnData>);
-};
+import { BeanBase, Cast, SymbolBeanFullName, Virtual } from 'zova';
+import { DefinedInitialQueryOptions, UndefinedInitialQueryOptions } from './types.js';
 
 @Virtual()
 export class BeanDataBase<TScopeModule = unknown> extends BeanBase<TScopeModule> {
@@ -46,32 +29,51 @@ export class BeanDataBase<TScopeModule = unknown> extends BeanBase<TScopeModule>
   ): UnwrapNestedRefs<UseQueryReturnType<TData, TError>>;
   $useQuery(params) {
     params = { ...params };
-    params.queryKey = this._forcePrefix(params.queryKey);
+    params.queryKey = this._forceQueryKeyPrefix(params.queryKey);
     return this.ctx.meta.util.instanceScope(() => {
       const data = useQuery(params);
       window.setTimeout(() => {
         this.$queryClient.setQueryData(params.queryKey, 500);
-        const query = this.$queryClient.getQueryCache().find({ queryKey: params.queryKey })!;
-        const prefix = `${this.app.config.env.appName}-query`;
-        const storageKey = `${prefix}-${query.queryHash}`;
-        // Persist if we have storage defined, we use timeout to get proper state to be persisted
-        setTimeout(async () => {
-          localStorage.setItem(
-            storageKey,
-            await JSON.stringify({
-              state: query.state,
-              queryKey: query.queryKey,
-              queryHash: query.queryHash,
-              buster: '',
-            }),
-          );
-        }, 0);
+        this.$persisterSave(params.queryKey);
       }, 2000);
       return data;
     });
   }
 
-  private _forcePrefix(queryKey: QueryKey): QueryKey {
+  $persisterSave(queryKey: QueryKey, sync?: boolean) {
+    const query = this.$queryFind({ queryKey });
+    if (!query) return;
+    const prefix = this._getPersisterPrefix();
+    const storageKey = `${prefix}-${query.queryHash}`;
+    if (sync === true) return;
+    // Persist if we have storage defined, we use timeout to get proper state to be persisted
+    setTimeout(async () => {
+      localStorage.setItem(
+        storageKey,
+        await JSON.stringify({
+          state: query.state,
+          queryKey: query.queryKey,
+          queryHash: query.queryHash,
+          buster: '',
+        }),
+      );
+    }, 0);
+  }
+
+  $queryFind<TQueryFnData = unknown, TError = DefaultError, TData = TQueryFnData>(
+    filters: QueryFilters,
+  ): Query<TQueryFnData, TError, TData> | undefined {
+    filters = { ...filters };
+    Cast(filters).queryKey = this._forceQueryKeyPrefix(Cast(filters).queryKey);
+    return this.$queryClient.getQueryCache().find(filters as any);
+  }
+
+  private _getPersisterPrefix() {
+    return `${this.app.config.env.appName}-query`;
+  }
+
+  private _forceQueryKeyPrefix(queryKey: QueryKey): QueryKey {
+    if (!queryKey) return queryKey;
     const prefix = queryKey[0];
     if (prefix && typeof prefix === 'string' && prefix.split('.').length === 3) return queryKey;
     return [this[SymbolBeanFullName]].concat(queryKey as any);
