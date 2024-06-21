dcacc9e3070a773e2b2a138901f32a0d5e75bd36
zhennann <zhen.nann@icloud.com>
2024-06-21 16:34:03 +0800

    Update beans.ts

--- zova-dev/src/suite/a-home/modules/home-user/src/resource/beans.ts
+++ zova-dev/src/suite/a-home/modules/home-user/src/resource/beans.ts
@@ -1,5 +1,4 @@
 export * from '../bean/data.userInfo.js';
-export * from '../bean/store.userInfo.js';
 import { DataUserInfo } from '../bean/data.userInfo.js';
 import 'zova';
 declare module 'zova' {
