7ccdd7f11a86d960f160e254649227a76b54adf2
zhennann <zhen.nann@icloud.com>
2024-06-22 08:59:57 +0800

    Update types.ts

--- zova-dev/src/suite-vendor/a-core/modules/a-router/src/types.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-router/src/types.ts
@@ -1,6 +1,6 @@
 import { IComponentLayoutRecord, TypePageSchemas } from 'zova';
 import { RouteComponent, RouteLocationNormalizedLoaded, RouteRecordRaw } from 'vue-router';
-import { StoreRouterLike } from './bean/store.router.js';
+import { BeanRouterLike } from './bean/bean.router.js';
 
 export type Lazy<T> = () => Promise<T>;
 export type IModuleRouteComponent = RouteComponent | Lazy<RouteComponent>;
@@ -17,7 +17,7 @@ declare module 'vue-router' {
 
 declare module 'zova' {
   export interface BeanBase {
-    $router: StoreRouterLike;
+    $router: BeanRouterLike;
   }
 
   export interface IModuleResource {
