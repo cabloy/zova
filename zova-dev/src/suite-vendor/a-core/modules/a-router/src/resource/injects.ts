62647b71563687e50881fa46a63c0b5e152ffe37
zhennann <zhen.nann@icloud.com>
2024-06-22 09:00:09 +0800

    Update injects.ts

--- zova-dev/src/suite-vendor/a-core/modules/a-router/src/resource/injects.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-router/src/resource/injects.ts
@@ -1,10 +1,10 @@
 import { Router } from 'vue-router';
-import { StoreRouterLike } from '../bean/store.router.js';
+import { BeanRouterLike } from '../bean/bean.router.js';
 
 import 'zova';
 declare module 'zova' {
   export interface IInjectRecord {
     'a-router:appRouter': Router;
-    'a-router:router': StoreRouterLike;
+    'a-router:router': BeanRouterLike;
   }
 }
