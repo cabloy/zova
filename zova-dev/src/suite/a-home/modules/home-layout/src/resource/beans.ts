4716d6c60fa679c300d8a533e2a38c2f143d1fe9
zhennann <zhen.nann@icloud.com>
2024-06-23 16:56:49 +0800

    feat: data layout

--- zova-dev/src/suite/a-home/modules/home-layout/src/resource/beans.ts
+++ zova-dev/src/suite/a-home/modules/home-layout/src/resource/beans.ts
@@ -1,4 +1,8 @@
+export * from '../bean/data.layout.js';
+import { DataLayout } from '../bean/data.layout.js';
 import 'zova';
 declare module 'zova' {
-  export interface IBeanRecord {}
+  export interface IBeanRecord {
+    'home-layout.data.layout': DataLayout;
+  }
 }
