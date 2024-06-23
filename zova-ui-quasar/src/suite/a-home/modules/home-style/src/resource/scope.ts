a0d6ce4ec1c50e895a7db6337cb4aec14ae91a03
zhennann <zhen.nann@icloud.com>
2024-06-23 16:45:37 +0800

    feat: api

--- zova-ui-quasar/src/suite/a-home/modules/home-style/src/resource/scope.ts
+++ zova-ui-quasar/src/suite/a-home/modules/home-style/src/resource/scope.ts
@@ -1,6 +1,7 @@
 import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'zova';
 import { config, Errors, locales, constants } from '../config/index.js';
 import { components } from './components.js';
+import { services } from '../api/service/index.js';
 
 @Scope()
 export class ScopeModuleHomeStyle extends BeanScopeBase {}
@@ -11,7 +12,8 @@ export interface ScopeModuleHomeStyle
     typeof config,
     typeof Errors,
     (typeof locales)[TypeLocaleBase],
-    typeof constants
+    typeof constants,
+    typeof services
   > {}
 
 import 'zova';
