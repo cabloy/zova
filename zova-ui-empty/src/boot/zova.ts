7836b3109721b78f1e89e0a2314e023072f8b13d
zhennann <zhen.nann@icloud.com>
2024-06-23 22:11:43 +0800

    feat: empty

--- zova-ui-empty/src/boot/zova.ts
+++ zova-ui-empty/src/boot/zova.ts
@@ -6,9 +6,7 @@ import { modulesMeta } from '../../.zova/modules-meta.js';
 import { AppMonkey } from '../front/config/monkey.js';
 import { locales } from '../front/config/locales.js';
 import config from '../../.zova/config.js';
-import { Router } from 'vue-router';
 
-export async function zova({ app, router }: { app: App; router: Router }) {
-  app.provide('a-router:appRouter', router);
+export async function zova({ app }: { app: App }) {
   await PluginZova.install(app, { modulesMeta, AppMonkey, locales, config });
 }
