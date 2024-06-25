0ecd747d0d7ff76c079d24a0ad26cb405e2eb82a
zhennann <zhen.nann@icloud.com>
2024-06-25 09:20:14 +0800

    feat: router.ts

--- zova-dev/packages-utils/zova-vite/templates/app/controller.ts
+++ zova-dev/packages-utils/zova-vite/templates/app/controller.ts
@@ -1,6 +1,6 @@
 import { getCurrentInstance } from 'vue';
 import { BeanControllerPageBase, Local, PluginZova } from 'zova';
-import createRouter from '../../src/boot/router.js';
+import createRouter from '../router.js';
 import { locales } from '../../src/front/config/locales.js';
 import { AppMonkey } from '../../src/front/config/monkey.js';
 import config from '../config.js';
@@ -13,12 +13,16 @@ export class ControllerPageApp extends BeanControllerPageBase {
     const instance = getCurrentInstance();
     const app = instance!.appContext.app!;
     // router
-    const router = createRouter();
+    const router = this.onCreateRouter();
     app.provide('a-router:appRouter', router);
     this.bean.provide('a-router:appRouter', router);
     // install
     await PluginZova.install(app, this.ctx.bean, { modulesMeta, AppMonkey, locales, config });
     // use router
     app.use(router);
+  }
+
+  protected onCreateRouter() {
+    return createRouter();
   }
 }
