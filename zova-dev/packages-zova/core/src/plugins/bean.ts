8275a62b1de8d5e4124f0c974f9b87cd26a895ed
zhennann <zhen.nann@icloud.com>
2024-06-26 17:34:19 +0800

    feat: component.activate

--- zova-dev/packages-zova/core/src/plugins/bean.ts
+++ zova-dev/packages-zova/core/src/plugins/bean.ts
@@ -1,30 +1,15 @@
 import { App } from 'vue';
 import { ZovaContext } from '../core/index.js';
-import { BeanRenderBase } from '../bean/beanRenderBase.js';
-import { BeanRenderIdentifier } from '../bean/type.js';
 
 export const PluginBean = {
   install(app: App) {
     app.mixin({
       created() {
-        const self = this;
-        // self._ is instance, not use self.$
-        const ctx: ZovaContext = self._.zova;
-        if (!ctx) return;
-        self.__bean_render_original = self._.render;
-        self._.render = function (this, ...args) {
-          if (self._.isUnmounted) return;
-          if (!ctx.meta.state.inited.state) {
-            return self.__bean_render_original.call(this, ...args);
-          }
-          const render: BeanRenderBase = ctx.bean._getBeanSyncOnly(BeanRenderIdentifier);
-          if (!render) {
-            return self.__bean_render_original.call(this, ...args);
-            // throw new Error('render bean not found');
-          }
-          // need not use ctx.meta.util.instanceScope, since ctx.instance = getCurrentInstance()
-          return render.render();
-        };
+        // this._ is instance, not use self.$
+        const ctx: ZovaContext = this._.zova;
+        if (ctx) {
+          ctx.meta.component.activate();
+        }
       },
     });
   },
