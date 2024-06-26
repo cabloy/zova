9d485a49c9fa70fdcaed0cf5df1ccb8859fc91e9
zhennann <zhen.nann@icloud.com>
2024-06-26 14:54:44 +0800

    Update state.ts

--- zova-dev/packages-zova/core/src/core/context/state.ts
+++ zova-dev/packages-zova/core/src/core/context/state.ts
@@ -1,13 +1,21 @@
 import { BeanSimple } from '../../bean/beanSimple.js';
 import { StateLock } from '../../utils/stateLock.js';
 
+let __id: number = 0;
+
 export class CtxState extends BeanSimple {
+  private _id: number;
   private _inited: StateLock;
   private _mounted: StateLock;
 
   protected __init__() {
+    this._id = ++__id;
     this._inited = StateLock.create();
     this._mounted = StateLock.create();
+  }
+
+  get id() {
+    return this._id;
   }
 
   get inited() {
