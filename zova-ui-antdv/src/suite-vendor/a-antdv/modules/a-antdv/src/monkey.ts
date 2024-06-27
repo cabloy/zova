12ebb94c5b9e3e1920961d2bdbfca168d58d041f
zhennann <zhen.nann@icloud.com>
2024-06-27 16:13:04 +0800

    feat: defaultThemeHandler

--- zova-ui-antdv/src/suite-vendor/a-antdv/modules/a-antdv/src/monkey.ts
+++ zova-ui-antdv/src/suite-vendor/a-antdv/modules/a-antdv/src/monkey.ts
@@ -1,8 +1,13 @@
 import { BeanBase, BeanContainer, BeanSimple, IMonkeySystem } from 'zova';
 import { App } from 'ant-design-vue';
+import { ScopeModuleAStyle } from 'zova-module-a-style';
 
 export class Monkey extends BeanSimple implements IMonkeySystem {
-  async appInitialize(_bean: BeanContainer) {}
+  async appInitialize(bean: BeanContainer) {
+    // defaultThemeHandler
+    const scopeStyle: ScopeModuleAStyle = await bean.getScope('a-style');
+    scopeStyle.config.defaultThemeHandler = 'a-antdv.tool.themeHandler';
+  }
   async appInitialized(_bean: BeanContainer) {}
   async beanInit(bean: BeanContainer, beanInstance: BeanBase) {
     bean.defineProperty(beanInstance, '$antdv', {
