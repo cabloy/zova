288fa49a9939794124430150c64b82f4a26139a6
zhennann <zhen.nann@icloud.com>
2024-06-30 22:00:39 +0800

    Update render.tsx

--- zova-dev/src/suite/a-home/modules/home-layout/src/component/layoutDefault/render.tsx
+++ zova-dev/src/suite/a-home/modules/home-layout/src/component/layoutDefault/render.tsx
@@ -37,6 +37,55 @@ export class RenderLayoutDefault extends BeanRenderBase {
   }
 
   render() {
+    <div class="drawer">
+      <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div class="navbar bg-base-300 w-full">
+          <div class="flex-none lg:hidden">
+            <label htmlFor="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block h-6 w-6 stroke-current"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+            </label>
+          </div>
+          <div class="mx-2 flex-1 px-2">Navbar Title</div>
+          <div class="hidden flex-none lg:block">
+            <ul class="menu menu-horizontal">
+              {/* Navbar menu content here */}
+              <li>
+                <a>Navbar Item 1</a>
+              </li>
+              <li>
+                <a>Navbar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Page content here */}
+        Content
+      </div>
+      <div class="drawer-side">
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu bg-base-200 min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>;
+  }
+
+  render1() {
     return (
       <div>
         <div>{this._renderMenu()}</div>
