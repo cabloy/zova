751bf0ecb75de1b89af7b35130b02b5bc8868b51
zhennann <zhen.nann@icloud.com>
2024-06-30 10:04:47 +0800

    feat: model defaultOptions

--- zova-dev/src/suite-vendor/a-core/modules/a-model/src/config/config.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-model/src/config/config.ts
@@ -1,4 +1,21 @@
+import { DefaultOptions, defaultShouldDehydrateQuery } from '@tanstack/vue-query';
 import { ZovaApplication } from 'zova';
+
+const defaultOptions: DefaultOptions = {
+  queries: {
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: true,
+    gcTime: 1000 * 60 * 5,
+  },
+  dehydrate: {
+    shouldDehydrateQuery(query) {
+      if (query.meta?.ssr?.dehydrate === false) return false;
+      return defaultShouldDehydrateQuery(query);
+    },
+  },
+};
 
 export const config = (_app: ZovaApplication) => {
   return {
@@ -9,6 +26,9 @@ export const config = (_app: ZovaApplication) => {
       async: {
         maxAge: 1000 * 60 * 60 * 24, // 24 hours
       },
+    },
+    queryClientConfig: {
+      defaultOptions,
     },
   };
 };
--- zova-dev/src/suite-vendor/a-core/modules/a-model/src/local/storage.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-model/src/local/storage.ts
@@ -1,27 +1,13 @@
 import { BeanBase, Local } from 'zova';
 import { ScopeModule } from '../resource/this.js';
-import { VueQueryPlugin, VueQueryPluginOptions, defaultShouldDehydrateQuery } from '@tanstack/vue-query';
+import { VueQueryPlugin, VueQueryPluginOptions } from '@tanstack/vue-query';
 
 @Local()
 export class Storage extends BeanBase<ScopeModule> {
   protected async __init__() {
     const vueQueryPluginOptions: VueQueryPluginOptions = {
       queryClientConfig: {
-        defaultOptions: {
-          queries: {
-            retry: false,
-            refetchOnWindowFocus: false,
-            refetchOnMount: false,
-            refetchOnReconnect: false,
-            // gcTime: 1000 * 60 * 5,
-          },
-          dehydrate: {
-            shouldDehydrateQuery(query) {
-              if (query.meta?.ssr?.dehydrate === false) return false;
-              return defaultShouldDehydrateQuery(query);
-            },
-          },
-        },
+        defaultOptions: this.scope.config.queryClientConfig.defaultOptions,
       },
     };
     this.app.vue.use(VueQueryPlugin, vueQueryPluginOptions);
