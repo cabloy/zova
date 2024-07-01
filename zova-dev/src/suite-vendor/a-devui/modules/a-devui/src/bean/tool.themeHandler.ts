d6b18ceb9010b6f95b08407c63cbe5a2df2445cf
zhennann <zhen.nann@icloud.com>
2024-07-01 16:12:09 +0800

    feat: theme

--- zova-dev/src/suite-vendor/a-devui/modules/a-devui/src/bean/tool.themeHandler.ts
+++ zova-dev/src/suite-vendor/a-devui/modules/a-devui/src/bean/tool.themeHandler.ts
@@ -2,9 +2,21 @@ import { BeanBase, Tool } from 'zova';
 import { ScopeModule } from '../resource/this.js';
 import { ThemeHandler, ThemeHandlerApplyParams } from 'zova-module-a-style';
 
+const __Themes = { 'a-demo.theme.orange': 'orange', 'home-theme.theme.default': '' };
+
 @Tool()
 export class ToolThemeHandler extends BeanBase<ScopeModule> implements ThemeHandler {
-  async apply({ token: _token }: ThemeHandlerApplyParams): Promise<void> {
-    // do nothing
+  async apply({ name, dark, token: _token }: ThemeHandlerApplyParams): Promise<void> {
+    // themeName
+    const _names: string[] = [];
+    const _name = __Themes[name];
+    if (_name) _names.push(_name);
+    _names.push(dark ? 'dark' : 'light');
+    const themeName = _names.join('-');
+    // data-theme
+    const body = window?.document?.documentElement;
+    if (body) {
+      body.setAttribute('data-theme', themeName);
+    }
   }
 }
--- zova-dev/tailwind.config.js
+++ zova-dev/tailwind.config.js
@@ -7,4 +7,24 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      {
+        'orange-light': {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#f28238',
+          'base-100': '#ffffff',
+        },
+      },
+      {
+        'orange-dark': {
+          ...require('daisyui/src/theming/themes')['dark'],
+          primary: '#f28238',
+          'base-100': '#121212',
+        },
+      },
+    ],
+  },
 };
