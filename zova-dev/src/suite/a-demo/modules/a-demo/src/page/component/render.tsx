79907b6db8db61b78d1a82bc45a385d1236cb3bc
zhennann <zhen.nann@icloud.com>
2024-07-01 11:09:53 +0800

    feat: component

--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/component/render.tsx
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/component/render.tsx
@@ -20,27 +20,32 @@ export class RenderComponent extends BeanRenderBase<ScopeModule> {
       },
     };
     return (
-      <div>
-        <this.scope.component.card
-          onControllerRef={ref => {
-            this.cardRef = ref;
-            console.log('cardRef.$props: ', this.cardRef?.$props);
-          }}
-          header="header"
-          content={this.resetTime.toString()}
-          footer="footer"
-          onReset={time => {
-            this.resetTime = time;
-          }}
-          v-slots={slots}
-        ></this.scope.component.card>
-        <input
-          ref={ref => {
-            this.inputRef = ref as any;
-            this.inputRef?.focus();
-          }}
-        ></input>
-      </div>
+      <this.$component.page>
+        <div>
+          <this.scope.component.card
+            onControllerRef={ref => {
+              this.cardRef = ref;
+              console.log('cardRef.$props: ', this.cardRef?.$props);
+            }}
+            header="header"
+            content={this.resetTime.toString()}
+            footer="footer"
+            onReset={time => {
+              this.resetTime = time;
+            }}
+            v-slots={slots}
+          ></this.scope.component.card>
+          <label>Input: </label>
+          <input
+            ref={ref => {
+              this.inputRef = ref as any;
+              this.inputRef?.focus();
+            }}
+            value={this.resetTime.toString()}
+            style={{ width: '450px' }}
+          ></input>
+        </div>
+      </this.$component.page>
     );
   }
 }
