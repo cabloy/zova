6fdc5f4d5f491fd1f3306e852b20226829e7c50e
zhennann <zhen.nann@icloud.com>
2024-07-01 15:09:45 +0800

    Update render.tsx

--- zova-dev/src/suite/a-home/modules/home-user/src/page/login/render.tsx
+++ zova-dev/src/suite/a-home/modules/home-user/src/page/login/render.tsx
@@ -76,31 +76,4 @@ export class RenderLogin extends BeanRenderBase<ScopeModule> {
       </div>
     );
   }
-
-  render1() {
-    return (
-      <div>
-        <form class={this.$class.textCenter}>
-          <div>
-            <label>{this.scope.locale.YourUsername()}</label>
-            <input v-model={this.user.username}></input>
-          </div>
-          <div>
-            <label>{this.scope.locale.YourPassword()}</label>
-            <input v-model={this.user.password} type="password"></input>
-          </div>
-          <div>
-            <button
-              type="submit"
-              onClick={withModifiers(() => {
-                this.login();
-              }, ['prevent'])}
-            >
-              {this.scope.locale.Login()}
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
 }
