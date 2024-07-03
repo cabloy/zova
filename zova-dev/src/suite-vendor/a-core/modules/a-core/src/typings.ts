27448e35cc01a5efa4485a8b9c6fabb7dc0ffbc3
zhennann <zhen.nann@icloud.com>
2024-07-03 21:42:25 +0800

    feat: a-tabs

--- zova-dev/src/suite-vendor/a-core/modules/a-core/src/typings.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-core/src/typings.ts
@@ -2,3 +2,4 @@ export type * as APinia from 'zova-module-a-pinia';
 export type * as ARouter from 'zova-module-a-router';
 export type * as AModel from 'zova-module-a-model';
 export type * as AStyle from 'zova-module-a-style';
+export type * as ATabs from 'zova-module-a-tabs';
