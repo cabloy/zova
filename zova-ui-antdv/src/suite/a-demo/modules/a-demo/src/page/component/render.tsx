f528951483255315314926632ed3bb1eba206b3f
zhennann <zhen.nann@icloud.com>
2024-07-04 13:43:15 +0800

    feat: v-slots

--- zova-ui-antdv/src/suite/a-demo/modules/a-demo/src/page/component/render.tsx
+++ zova-ui-antdv/src/suite/a-demo/modules/a-demo/src/page/component/render.tsx
@@ -1,23 +1,11 @@
 import { BeanRenderBase, Local } from 'zova';
 import type { ControllerPageComponent } from './controller.js';
-import { NSControllerCard } from '../../resource/components.js';
 import { ScopeModule } from '../../resource/this.js';
 export interface RenderComponent extends ControllerPageComponent {}
 
 @Local()
 export class RenderComponent extends BeanRenderBase<ScopeModule> {
   render() {
-    const slots: NSControllerCard.Slots = {
-      header: () => {
-        return <div>this is a header slot from parent</div>;
-      },
-      default: () => {
-        return <div>this is a default slot from parent</div>;
-      },
-      footer: () => {
-        return <div>this is a footer slot from parent</div>;
-      },
-    };
     return (
       <div>
         <this.scope.component.card
@@ -30,7 +18,17 @@ export class RenderComponent extends BeanRenderBase<ScopeModule> {
           onReset={time => {
             this.resetTime = time;
           }}
-          v-slots={slots}
+          slots={{
+            header: () => {
+              return <div>this is a header slot from parent</div>;
+            },
+            default: () => {
+              return <div>this is a default slot from parent</div>;
+            },
+            footer: () => {
+              return <div>this is a footer slot from parent</div>;
+            },
+          }}
         ></this.scope.component.card>
       </div>
     );
