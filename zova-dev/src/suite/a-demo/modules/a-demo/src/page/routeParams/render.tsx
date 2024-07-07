a1348c4e9b86c33fa87bb228f85b39d3343f149a
zhennann <zhen.nann@icloud.com>
2024-07-07 21:10:33 +0800

    Update render.tsx

--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/routeParams/render.tsx
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/routeParams/render.tsx
@@ -9,7 +9,36 @@ export class RenderRouteParams extends BeanRenderBase<ScopeModule> {
   render() {
     return (
       <this.$component.page>
-        <div>{this.$params.id}</div>
+        <div class="overflow-x-auto">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>$params.id</td>
+                <td>{this.$params.id}</td>
+                <td>{typeof this.$params.id}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button
+            class="btn btn-primary"
+            onClick={() => {
+              const id = this.$params.id + 1;
+              const url = this.$router.resolveName('a-demo:routeParams', {
+                params: { id },
+              });
+              this.$router.push(url);
+            }}
+          >
+            Go to current page with different params value
+          </button>
+        </div>
       </this.$component.page>
     );
   }
