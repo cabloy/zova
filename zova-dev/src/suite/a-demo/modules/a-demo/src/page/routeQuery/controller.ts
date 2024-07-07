e5ab8690c215dd1aa079308a848bd02dc14e6a62
zhennann <zhen.nann@icloud.com>
2024-07-07 17:00:55 +0800

    feat: query

--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/routeQuery/controller.ts
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/routeQuery/controller.ts
@@ -5,7 +5,10 @@ export const ParamsSchema = zz.object({});
 export type ParamsInput = zz.input<typeof ParamsSchema>;
 export type ParamsOutput = zz.output<typeof ParamsSchema>;
 
-export const QuerySchema = zz.object({});
+export const QuerySchema = zz.object({
+  name: zz.string().optional(),
+  age: zz.number().optional(),
+});
 export type QueryInput = zz.input<typeof QuerySchema>;
 export type QueryOutput = zz.output<typeof QuerySchema>;
 
--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/routeQuery/render.tsx
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/routeQuery/render.tsx
@@ -9,7 +9,43 @@ export class RenderRouteQuery extends BeanRenderBase<ScopeModule> {
   render() {
     return (
       <this.$component.page>
-        <div>to be done</div>
+        <div class="overflow-x-auto">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>this.$query.name</td>
+                <td>{this.$query.name}</td>
+                <td>{typeof this.$query.name}</td>
+              </tr>
+              <tr>
+                <td>this.$query.age</td>
+                <td>{this.$query.age}</td>
+                <td>{typeof this.$query.age}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button
+            class="btn btn-primary"
+            onClick={() => {
+              const name = this.$query.name === 'tom' ? 'kevin' : 'tom';
+              const age = (this.$query.age ?? 0) + 1;
+              const url = this.$router.resolvePath('/a/demo/routeQuery', {
+                name,
+                age,
+              });
+              this.$router.push(url);
+            }}
+          >
+            Go to current page with different query value
+          </button>
+        </div>
       </this.$component.page>
     );
   }
