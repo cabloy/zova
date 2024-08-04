3ab304e111b1effc9d78bdfa48f07c6999b0eb94
zhennann <zhen.nann@icloud.com>
2024-08-04 11:10:50 +0800

    Update routes.ts

--- zova-dev/src/suite/a-home/modules/home-pagesystem/src/routes.ts
+++ zova-dev/src/suite/a-home/modules/home-pagesystem/src/routes.ts
@@ -2,13 +2,7 @@ import ErrorNotFound from './page/errorNotFound/index.vue';
 import { IModuleRoute } from 'zova-module-a-router';
 
 export const routes: IModuleRoute[] = [
-  {
-    path: '/',
-    redirect: '/a/home/home',
-    meta: {
-      absolute: true,
-    },
-  },
+  // { path: '/', redirect: '/a/home/home', meta: { absolute: true } },
   {
     path: '/:catchAll(.*)*',
     component: ErrorNotFound,
