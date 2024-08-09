bf21ac4af60e6a2c7db93dcef6abdfaea8ce8d62
zhennann <zhen.nann@icloud.com>
2024-08-09 13:52:34 +0800

    Update router.ts

--- zova-ui-quasar/src/boot/router.ts
+++ zova-ui-quasar/src/boot/router.ts
@@ -1,5 +1,15 @@
 export default function () {
   return {
     install() {},
+    resolve(url) {
+      return { fullPath: url };
+    },
+    async push(_url) {},
+    async isReady() {},
+    currentRoute: {
+      value: {
+        matched: [{ components: [{}] }],
+      },
+    },
   };
 }
