7519f40dcb334a74e02f31084477717bae39fd7f
zhennann <zhen.nann@icloud.com>
2024-08-12 22:57:41 +0800

    Update extendViteConf.ts

--- zova-ui-quasar/packages-utils/quasar-app-extension-zova/src/quasar/extendViteConf.ts
+++ zova-ui-quasar/packages-utils/quasar-app-extension-zova/src/quasar/extendViteConf.ts
@@ -7,9 +7,14 @@ export function extendViteConf(context: ConfigContext) {
     const zovaViteMeta = context.zovaViteMeta as ZovaViteConfigResult;
     // conf.build override zovaViteMeta.viteConfig.build
     const minify = conf.build.minify;
+    // have two outDir for ssr
+    const outDir = conf.build.outDir;
     conf.build = mergeConfig(conf.build as unknown as any, zovaViteMeta.viteConfig.build);
     if (minify === false) {
       conf.build.minify = minify;
+    }
+    if (outDir) {
+      conf.build.outDir = outDir;
     }
     // hmr
     if (opts.isClient) {
