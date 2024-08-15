b8e4cfa6acdb67ac48a148dc8a802b300094c72f
zhennann <zhen.nann@icloud.com>
2024-08-15 15:49:54 +0800

    feat: onHydratePropHasMismatch

--- zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/monkey.ts
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/monkey.ts
@@ -1,6 +1,7 @@
 import { BeanBase, BeanContainer, BeanSimple, Cast, IMonkeySystem } from 'zova';
 import { PatchIcon } from './patch/icon.js';
 import { ScopeModuleAStyle } from 'zova-module-a-style';
+import { normalizeClass } from 'vue';
 
 export class Monkey extends BeanSimple implements IMonkeySystem {
   async appInitialize(bean: BeanContainer) {
@@ -14,6 +15,17 @@ export class Monkey extends BeanSimple implements IMonkeySystem {
     if (process.env.CLIENT) {
       this.ctx.meta.ssr.onHydrated(() => {
         Cast(this.app.vue.config.globalProperties.$q).onSSRHydrated();
+      });
+      this.ctx.meta.ssr.onHydratePropHasMismatch((_el, key, clientValue, _vnode, _instance) => {
+        if (key !== 'class') return clientValue;
+        clientValue = normalizeClass(clientValue);
+        if (!clientValue) return clientValue;
+        return clientValue
+          .split(' ')
+          .filter(item => {
+            return item.indexOf('dark') === -1;
+          })
+          .join(' ');
       });
     }
     // ssr theme
