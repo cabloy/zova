91207db208422df0cd4defa511dc960e0fde22b7
zhennann <zhen.nann@icloud.com>
2024-08-16 22:02:48 +0800

    feat: model layout

--- zova-ui-quasar/src/suite/a-home/modules/home-layout/src/resource/beans.ts
+++ zova-ui-quasar/src/suite/a-home/modules/home-layout/src/resource/beans.ts
@@ -1,8 +1,11 @@
+export * from '../bean/model.layout.js';
 export * from '../bean/model.menu.js';
 import { ModelMenu } from '../bean/model.menu.js';
+import { ModelLayout } from '../bean/model.layout.js';
 import 'zova';
 declare module 'zova' {
   export interface IBeanRecord {
     'home-layout.model.menu': ModelMenu;
+    'home-layout.model.layout': ModelLayout;
   }
 }
