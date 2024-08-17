550ce76460f319d61c87359e1f51c6a5af3bb772
zhennann <zhen.nann@icloud.com>
2024-08-17 17:00:58 +0800

    Update monkey.ts

--- zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/monkey.ts
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/monkey.ts
@@ -24,14 +24,41 @@ export class Monkey extends BeanSimple implements IMonkeySystem {
   __prefersColorSchemeDarkLocal=__prefersColorSchemeDarkLocal?JSON.parse(__prefersColorSchemeDarkLocal):null;      
   var __prefersColorSchemeDark=__prefersColorSchemeDarkLocal??window.matchMedia('(prefers-color-scheme: dark)').matches;     
   if(__prefersColorSchemeDark){
-    document.body.classList.remove('body--light')
-    document.body.classList.add('body--dark')
+    document.body.classList.remove('body--light');
+    document.body.classList.add('body--dark');
   }else{
-    document.body.classList.remove('body--dark')
-    document.body.classList.add('body--light') 
+    document.body.classList.remove('body--dark');
+    document.body.classList.add('body--light');
   }
   document.currentScript.remove();
-</script>"`;
+</script>`;
+        this.ctx.meta.ssr.context._meta.bodyTags += `<script id="__leftDrawerOpenJS">
+  window.onload=function(){
+  debugger;
+    var __belowBreakpoint=document.body.offsetWidth <= 1023;
+    var __leftDrawerOpen;
+    if(__belowBreakpoint){
+      __leftDrawerOpen=false;
+    }else{
+      var __leftDrawerOpenPC=localStorage.getItem('leftDrawerOpenPC');
+      __leftDrawerOpen=__leftDrawerOpenPC?JSON.parse(__leftDrawerOpenPC):true;  
+    }
+    if(__leftDrawerOpen){
+      var __domHeader=document.querySelector('#q-app>.q-layout>.q-header');
+      var __domDrawer=document.querySelector('#q-app>.q-layout>.q-drawer-container>.q-drawer--left');
+      var __domPageContainer=document.querySelector('#q-app>.q-layout>.q-page-container');
+      __domHeader.style.left='300px';
+      __domHeader.setAttribute('data-hydrate-props-server-first','');
+      __domDrawer.style.transform='translateX(0px)';
+      __domDrawer.style.left='0px';
+      __domDrawer.className=__domDrawer.className.replace('q-layout--prevent-focus ','');
+      __domDrawer.setAttribute('data-hydrate-props-server-first','');
+      __domPageContainer.style.paddingLeft='300px'; 
+      __domPageContainer.setAttribute('data-hydrate-props-server-first','');
+    }
+    document.querySelector('#__leftDrawerOpenJS').remove();
+  };      
+</script>`;
       });
     }
   }
