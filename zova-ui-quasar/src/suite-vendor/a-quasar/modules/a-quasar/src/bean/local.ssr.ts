271bd4602e919fc56e8b1a917722c8dac3e067d2
zhennann <zhen.nann@icloud.com>
2024-08-19 15:51:00 +0800

    feat: ssr

---
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/bean/local.ssr.ts
@@ -0,0 +1,58 @@
+import { BeanBase, Cast, Local } from 'zova';
+import { ScopeModule } from '../resource/this.js';
+
+@Local()
+export class LocalSSR extends BeanBase<ScopeModule> {
+  public async initialize() {
+    // ssr hydrated
+    if (process.env.CLIENT) {
+      this.ctx.meta.ssr.onHydrated(() => {
+        Cast(this.app.vue.config.globalProperties.$q).onSSRHydrated();
+      });
+    }
+    // ssr theme
+    if (process.env.SERVER) {
+      this.ctx.meta.ssr.context.onRendered(() => {
+        this.ctx.meta.ssr.context._meta.bodyTags += `<script id="__prefersColorSchemeDarkJS">
+    if(window.__prefersColorSchemeDark){
+      document.body.classList.remove('body--light');
+      document.body.classList.add('body--dark');
+    }else{
+      document.body.classList.remove('body--dark');
+      document.body.classList.add('body--light');
+    }
+    document.querySelector('#__prefersColorSchemeDarkJS').remove();
+</script>`.replaceAll('\n', '');
+        if (this.app.config.ssr.optimization.bodyHiddenBeforeLoad) {
+          this.ctx.meta.ssr.context._meta.bodyTags += `<script id="__leftDrawerOpenJS">
+  document.addEventListener("DOMContentLoaded", () => {
+    var __belowBreakpoint=document.documentElement.clientWidth <= 1023;
+    var __leftDrawerOpen;
+    if(__belowBreakpoint){
+      __leftDrawerOpen=false;
+    }else{
+      var __leftDrawerOpenPC=localStorage.getItem('leftDrawerOpenPC');
+      __leftDrawerOpen=__leftDrawerOpenPC?JSON.parse(__leftDrawerOpenPC):${this.scope.config.layout.leftDrawerOpenPC};  
+    }
+    if(__leftDrawerOpen){
+      var __domHeader=document.querySelector('#q-app>.q-layout>.q-header');
+      var __domDrawer=document.querySelector('#q-app>.q-layout>.q-drawer-container>.q-drawer--left');
+      var __domPageContainer=document.querySelector('#q-app>.q-layout>.q-page-container');
+      __domHeader.style.left='300px';
+      __domDrawer.style.transform='unset !important';
+      __domDrawer.className=__domDrawer.className.replace('q-layout--prevent-focus ','');
+      __domPageContainer.style.paddingLeft='300px';
+      if(window.__prefersColorSchemeDark){
+      __domDrawer.classList.add('q-drawer--dark','q-dark');
+      __domDrawer.querySelector('.q-list').classList.add('q-list--dark');
+      __domDrawer.querySelector('.q-separator').classList.add('q-separator--dark');
+      }
+    }
+    document.querySelector('#__leftDrawerOpenJS').remove();
+  });
+</script>`.replaceAll('\n', '');
+        }
+      });
+    }
+  }
+}
--- zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/monkey.ts
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/monkey.ts
@@ -1,6 +1,7 @@
-import { BeanBase, BeanContainer, BeanSimple, Cast, IMonkeySystem } from 'zova';
-import { PatchIcon } from './patch/icon.js';
+import { BeanBase, BeanContainer, BeanSimple, IMonkeySystem } from 'zova';
+import { LocalIcon } from './bean/local.icon.js';
 import { ScopeModuleAStyle } from 'zova-module-a-style';
+import { LocalSSR } from './bean/local.ssr.js';
 
 export class Monkey extends BeanSimple implements IMonkeySystem {
   async appInitialize(bean: BeanContainer) {
@@ -8,58 +9,11 @@ export class Monkey extends BeanSimple implements IMonkeySystem {
     const scopeStyle: ScopeModuleAStyle = await bean.getScope('a-style');
     scopeStyle.config.defaultThemeHandler = 'a-quasar.tool.themeHandler';
     // icon
-    const patchIcon = await bean._newBean(PatchIcon, false);
-    await patchIcon.initialize();
-    // ssr hydrated
-    if (process.env.CLIENT) {
-      this.ctx.meta.ssr.onHydrated(() => {
-        Cast(this.app.vue.config.globalProperties.$q).onSSRHydrated();
-      });
-    }
-    // ssr theme
-    if (process.env.SERVER) {
-      this.ctx.meta.ssr.context.onRendered(() => {
-        this.ctx.meta.ssr.context._meta.bodyTags += `<script id="__prefersColorSchemeDarkJS">
-    if(window.__prefersColorSchemeDark){
-      document.body.classList.remove('body--light');
-      document.body.classList.add('body--dark');
-    }else{
-      document.body.classList.remove('body--dark');
-      document.body.classList.add('body--light');
-    }
-    document.querySelector('#__prefersColorSchemeDarkJS').remove();
-</script>`.replaceAll('\n', '');
-        if (this.app.config.ssr.optimization.bodyHiddenBeforeLoad) {
-          this.ctx.meta.ssr.context._meta.bodyTags += `<script id="__leftDrawerOpenJS">
-  document.addEventListener("DOMContentLoaded", () => {
-    var __belowBreakpoint=document.documentElement.clientWidth <= 1023;
-    var __leftDrawerOpen;
-    if(__belowBreakpoint){
-      __leftDrawerOpen=false;
-    }else{
-      var __leftDrawerOpenPC=localStorage.getItem('leftDrawerOpenPC');
-      __leftDrawerOpen=__leftDrawerOpenPC?JSON.parse(__leftDrawerOpenPC):true;  
-    }
-    if(__leftDrawerOpen){
-      var __domHeader=document.querySelector('#q-app>.q-layout>.q-header');
-      var __domDrawer=document.querySelector('#q-app>.q-layout>.q-drawer-container>.q-drawer--left');
-      var __domPageContainer=document.querySelector('#q-app>.q-layout>.q-page-container');
-      __domHeader.style.left='300px';
-      __domDrawer.style.transform='unset !important';
-      __domDrawer.className=__domDrawer.className.replace('q-layout--prevent-focus ','');
-      __domPageContainer.style.paddingLeft='300px';
-      if(window.__prefersColorSchemeDark){
-      __domDrawer.classList.add('q-drawer--dark','q-dark');
-      __domDrawer.querySelector('.q-list').classList.add('q-list--dark');
-      __domDrawer.querySelector('.q-separator').classList.add('q-separator--dark');
-      }
-    }
-    document.querySelector('#__leftDrawerOpenJS').remove();
-  });
-</script>`.replaceAll('\n', '');
-        }
-      });
-    }
+    const localIcon = await bean._newBean(LocalIcon, false);
+    await localIcon.initialize();
+    // ssr
+    const localSSR = await bean._newBean(LocalSSR, false);
+    await localSSR.initialize();
   }
   async appInitialized(_bean: BeanContainer) {}
   async appReady(_bean: BeanContainer) {}
