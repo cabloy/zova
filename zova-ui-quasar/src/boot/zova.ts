8369e88d8bddeb1a79366db404d86fa4df6ba3ba
zhennann <zhen.nann@icloud.com>
2024-08-19 13:30:14 +0800

    Update zova.ts

--- zova-ui-quasar/src/boot/zova.ts
+++ zova-ui-quasar/src/boot/zova.ts
@@ -3,7 +3,4 @@ import { PluginBean } from 'zova';
 
 export default async function ({ app }: { app: App }) {
   app.use(PluginBean);
-  if (process.env.CLIENT && document.body.getAttribute('data-server-rendered') !== null) {
-    app.config.globalProperties.$q.dark.set((<any>window).__prefersColorSchemeDark);
-  }
 }
