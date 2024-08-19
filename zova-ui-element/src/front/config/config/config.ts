78b620ea9b1d07391f4d0e7132ae7352dda89e92
zhennann <zhen.nann@icloud.com>
2024-08-19 16:30:33 +0800

    feat: config

--- zova-ui-element/src/front/config/config/config.ts
+++ zova-ui-element/src/front/config/config/config.ts
@@ -1,14 +1,7 @@
 import { ZovaConfigMeta, ZovaConfigOptional } from 'zova';
 
 export default function (_meta: ZovaConfigMeta) {
-  const config: ZovaConfigOptional = {
-    base: {},
-    api: {
-      baseURL: process.env.API_BASE_URL,
-      prefix: process.env.API_PREFIX,
-    },
-    layout: {},
-  };
+  const config: ZovaConfigOptional = {};
 
   // routes
   config.routes = {
