533ebe3e2b5e47a251e02248610613e7e264b1d0
zhennann <zhen.nann@icloud.com>
2024-08-20 10:04:53 +0800

    Update extendFiles.ts

--- zova-ui-quasar/packages-utils/quasar-app-extension-zova/src/quasar/extendFiles.ts
+++ zova-ui-quasar/packages-utils/quasar-app-extension-zova/src/quasar/extendFiles.ts
@@ -4,18 +4,22 @@ import { QuasarConf } from '@quasar/app-vite/types/configuration/conf.js';
 import { IndexAPI } from '@quasar/app-vite';
 
 export function extendFiles(_context: ConfigContext) {
-  return async function extendFiles(_conf: QuasarConf, api: IndexAPI) {
+  return async function extendFiles(conf: QuasarConf, api: IndexAPI) {
+    // patch templates
+    await patchTemplates(conf, api);
+  };
+
+  async function patchTemplates(_conf: QuasarConf, api: IndexAPI) {
+    // client-entry.js
     const clientEntryFile = api.resolve.cli('templates/entry/client-entry.js');
     const clientEntryContent = (await fse.readFile(clientEntryFile)).toString();
     if (clientEntryContent.indexOf('quasarUserOptions.config.dark') === -1) {
       const clientEntryContentNew = clientEntryContent.replace(
         'async function start',
         `quasarUserOptions.config.dark = window.__prefersColorSchemeDark
-  async function start`,
+    async function start`,
       );
       await fse.outputFile(clientEntryFile, clientEntryContentNew);
     }
-  };
+  }
 }
-
-function patchTemplates() {}
