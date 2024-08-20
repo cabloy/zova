3bc57dac38e186b82433acf2e4bad0b9810d8a6b
zhennann <zhen.nann@icloud.com>
2024-08-20 14:16:00 +0800

    feat: ignore

---
+++ zova-dev/quasar.config.ts
@@ -0,0 +1,41 @@
+/* eslint-env node */
+
+// Configuration for your app
+// https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
+
+import { configure } from 'quasar/wrappers';
+
+export default configure(_ctx => {
+  return {
+    zovaManualChunk: {
+      debug: false,
+      vendors: [],
+    },
+    css: ['app.scss'],
+    build: {
+      target: {
+        browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
+        node: 'node20',
+      },
+
+      // analyze: true,
+      // minify: false,
+
+      // extendViteConf(_viteConf) {},
+      // viteVuePluginOptions: {},
+    },
+
+    devServer: {
+      // https: true
+      open: false, // opens browser window automatically
+    },
+
+    ssr: {
+      middlewares: [
+        'render', // keep this as last one
+      ],
+      // extendPackageJson (json) {},
+      // extendSSRWebserverConf (esbuildConf) {},
+    },
+  };
+});
