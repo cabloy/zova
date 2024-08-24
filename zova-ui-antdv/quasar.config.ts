6a49cca77a65e002f9ed9fe817991cbd2a0b4ab7
zhennann <zhen.nann@icloud.com>
2024-08-24 13:54:44 +0800

    Update quasar.config.ts

--- zova-ui-antdv/quasar.config.ts
+++ zova-ui-antdv/quasar.config.ts
@@ -4,6 +4,7 @@
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
 import { configure } from 'quasar/wrappers';
+import { mergeConfig } from 'vite';
 
 export default configure(_ctx => {
   return {
@@ -13,7 +14,13 @@ export default configure(_ctx => {
     },
 
     build: {
-      // extendViteConf(_viteConf) {},
+      extendViteConf(viteConf) {
+        viteConf.ssr = mergeConfig(viteConf.ssr || {}, {
+          //noExternal: ['ant-design-vue', 'scroll-into-view-if-needed', 'vue'],
+          //noExternal: ['ant-design-vue/es/theme/themes/default/colorAlgorithm.js', '@ctrl/tinycolor'],
+        });
+        console.log(viteConf.ssr);
+      },
       // viteVuePluginOptions: {},
     },
 
