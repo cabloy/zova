140bedb0e4a793c76c4efb294cb9adb7cc57b087
zhennann <zhen.nann@icloud.com>
2024-08-27 16:30:09 +0800

    Update tool.themeHandler.ts

--- zova-ui-element/src/suite-vendor/a-element/modules/a-element/src/bean/tool.themeHandler.ts
+++ zova-ui-element/src/suite-vendor/a-element/modules/a-element/src/bean/tool.themeHandler.ts
@@ -4,18 +4,28 @@ import { ThemeHandler, ThemeHandlerApplyParams } from 'zova-module-a-style';
 
 @Tool()
 export class ToolThemeHandler extends BeanBase<ScopeModule> implements ThemeHandler {
+  classBrand: string;
+
   async apply({ dark, token }: ThemeHandlerApplyParams): Promise<void> {
-    const body = window?.document?.documentElement;
-    if (body) {
-      for (const key in token) {
-        const key2 = `--el-${key}`;
-        body.style.setProperty(key2, token[key]);
-      }
-      if (dark) {
-        body.classList.add('dark');
-      } else {
-        body.classList.remove('dark');
-      }
+    // body
+    const body = process.env.SERVER ? undefined : window?.document?.documentElement;
+    // classBrand
+    if (this.classBrand) {
+      body?.classList.remove(this.classBrand);
+    }
+    const brand = {};
+    for (const key in token) {
+      const key2 = `--el-${key}`;
+      brand[key2] = token[key];
+      //body.style.setProperty(key2, token[key]);
+    }
+    this.classBrand = this.$style(brand);
+    body?.classList.add(this.classBrand);
+    // dark
+    if (dark) {
+      body?.classList.add('dark');
+    } else {
+      body?.classList.remove('dark');
     }
   }
 }
