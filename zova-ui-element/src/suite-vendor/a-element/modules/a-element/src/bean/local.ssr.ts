69756a532f98e2cc00f1f0ea3b6e6e69b6c94935
zhennann <zhen.nann@icloud.com>
2024-08-27 22:27:54 +0800

    feat: update

--- zova-ui-element/src/suite-vendor/a-element/modules/a-element/src/bean/local.ssr.ts
+++ zova-ui-element/src/suite-vendor/a-element/modules/a-element/src/bean/local.ssr.ts
@@ -1,9 +1,17 @@
 import { BeanBase, Local } from 'zova';
 import { ScopeModule } from '../resource/this.js';
+import { ID_INJECTION_KEY } from 'element-plus';
 
 @Local()
 export class LocalSSR extends BeanBase<ScopeModule> {
   public async initialize() {
+    // provide id
+    if (process.env.SSR) {
+      this.app.vue.provide(ID_INJECTION_KEY, {
+        prefix: 1024,
+        current: 0,
+      });
+    }
     // ssr style
     if (process.env.SERVER) {
       this.ctx.meta.ssr.context.onRendered(() => {});
