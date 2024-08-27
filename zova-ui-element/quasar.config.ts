3fb3e15dcb28224a2368947e2de2bb28f8027370
zhennann <zhen.nann@icloud.com>
2024-08-27 15:21:57 +0800

    feat: quasar

--- zova-ui-element/quasar.config.ts
+++ zova-ui-element/quasar.config.ts
@@ -10,12 +10,43 @@ import Unocss from 'unocss/vite';
 import { presetAttributify, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss';
 
 import { configure } from 'quasar/wrappers';
+import { mergeConfig } from 'vite';
 
 export default configure(_ctx => {
   return {
     build: {
       extendViteConf(viteConf) {
-        //viteConf.
+        // plugins
+        viteConf.plugins!.concat([
+          ElementPlus({}), // https://github.com/antfu/unocss
+          // see unocss.config.ts for config
+          (<any>Unocss)({
+            presets: [
+              presetUno(),
+              presetAttributify(),
+              presetIcons({
+                scale: 1.2,
+                warn: true,
+              }),
+            ],
+            transformers: [transformerDirectives(), transformerVariantGroup()],
+          }),
+        ]);
+        // resolve
+        const pathSrc = path.resolve(__dirname, 'src');
+        viteConf.resolve = mergeConfig(viteConf.resolve || {}, {
+          alias: {
+            '~/': `${pathSrc}/`,
+          },
+        });
+        // css
+        viteConf.css = mergeConfig(viteConf.css || {}, {
+          preprocessorOptions: {
+            scss: {
+              additionalData: '@use "~/css/element/index.scss" as *;',
+            },
+          },
+        });
       },
       // viteVuePluginOptions: {},
     },
