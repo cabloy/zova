68de266586221001f463fda9d5b1a36f666ed85c
zhennann <zhen.nann@icloud.com>
2024-08-27 12:54:08 +0800

    feat: remove roboto-font

--- zova-ui-quasar/quasar.config.ts
+++ zova-ui-quasar/quasar.config.ts
@@ -8,9 +8,6 @@ import { configure } from 'quasar/wrappers';
 export default configure(_ctx => {
   return {
     css: ['app.scss'],
-    extras: [
-      'roboto-font', // optional, you are not bound to it
-    ],
     build: {
       // extendViteConf(_viteConf) {},
       // viteVuePluginOptions: {},
@@ -18,64 +15,6 @@ export default configure(_ctx => {
 
     devServer: {
       open: false, // opens browser window automatically
-    },
-
-    animations: [],
-
-    ssr: {},
-
-    // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
-    pwa: {
-      workboxMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-cordova-apps/configuring-cordova
-    cordova: {},
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
-    capacitor: {
-      hideSplashscreen: true,
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
-    electron: {
-      // extendElectronMainConf (esbuildConf) {},
-      // extendElectronPreloadConf (esbuildConf) {},
-
-      // extendPackageJson (json) {},
-
-      // Electron preload scripts (if any) from /src-electron, WITHOUT file extension
-      preloadScripts: ['electron-preload'],
-
-      // specify the debugging port to use for the Electron app when running in development mode
-      inspectPort: 5858,
-
-      bundler: 'packager', // 'packager' or 'builder'
-
-      packager: {
-        // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-        // Windows only
-        // win32metadata: { ... }
-      },
-
-      builder: {
-        // https://www.electron.build/configuration/configuration
-
-        appId: 'quasar-project',
-      },
-    },
-
-    // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
-    bex: {
-      // extendBexScriptsConf (esbuildConf) {},
-      // extendBexManifestJson (json) {},
-
-      contentScripts: ['my-content-script'],
     },
   };
 });
