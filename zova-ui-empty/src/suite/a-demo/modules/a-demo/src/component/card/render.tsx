f644633cd0cd992a6a9353a582159ae1ba778087
zhennann <zhen.nann@icloud.com>
2024-08-28 23:17:29 +0800

    feat: component ssr

--- zova-ui-empty/src/suite/a-demo/modules/a-demo/src/component/card/render.tsx
+++ zova-ui-empty/src/suite/a-demo/modules/a-demo/src/component/card/render.tsx
@@ -17,16 +17,25 @@ export class RenderCard extends BeanRenderBase {
         </button>
         <div>
           <div style={{ backgroundColor: 'teal' }}>
-            <div>Slot: {this.$slots.header?.()}</div>
-            <div>Prop: {this.$props.header}</div>
+            <div>
+              <div>Slot:</div>
+              {this.$slots.header?.()}
+            </div>
+            <div>{`Prop: ${this.$props.header}`}</div>
           </div>
           <div style={{ backgroundColor: 'orange' }}>
-            <div>Slot: {this.$slots.default?.()}</div>
-            <div>Prop: {this.$props.content}</div>
+            <div>
+              <div>Slot:</div>
+              {this.$slots.default?.()}
+            </div>
+            <div>{`Prop: ${this.$props.content}`}</div>
           </div>
           <div style={{ backgroundColor: 'green' }}>
-            <div>Slot: {this.$slots.footer?.()}</div>
-            <div>Prop: {this.$props.footer}</div>
+            <div>
+              <div>Slot</div>
+              {this.$slots.footer?.()}
+            </div>
+            <div>{`Prop: ${this.$props.footer}`}</div>
           </div>
         </div>
       </div>
