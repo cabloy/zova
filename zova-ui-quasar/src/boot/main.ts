a8ff6d41623074137f9dac93eb1a32d79c86d95c
zhennann <zhen.nann@icloud.com>
2024-08-29 19:36:06 +0800

    feat: quasar

--- zova-ui-quasar/src/boot/main.ts
+++ zova-ui-quasar/src/boot/main.ts
@@ -7,7 +7,7 @@ import '../css/app.scss';
 export default (app: App, ssrContext: SSRContext) => {
   const quasarUserOptions = {
     config: {
-      dark: process.env.CLIENT ? window.ssr_themedark : undefined,
+      dark: process.env.CLIENT ? window.ssr_themedark : 'auto',
     },
   };
   Cast(app.use)(Quasar, quasarUserOptions, ssrContext);
