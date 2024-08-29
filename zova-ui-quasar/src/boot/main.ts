f86b44bd07631b7ee6407545b3f3cabe02054997
zhennann <zhen.nann@icloud.com>
2024-08-29 19:30:09 +0800

    feat: ssr_cookie_themedark

--- zova-ui-quasar/src/boot/main.ts
+++ zova-ui-quasar/src/boot/main.ts
@@ -7,7 +7,7 @@ import '../css/app.scss';
 export default (app: App, ssrContext: SSRContext) => {
   const quasarUserOptions = {
     config: {
-      dark: process.env.CLIENT ? window.ssr_local_themedark : undefined,
+      dark: process.env.CLIENT ? window.ssr_themedark : undefined,
     },
   };
   Cast(app.use)(Quasar, quasarUserOptions, ssrContext);
