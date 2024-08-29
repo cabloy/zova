c3b787f417973ff9ef23b34c8b237e4ecaa31333
zhennann <zhen.nann@icloud.com>
2024-08-29 19:52:45 +0800

    feat: quasar config

--- zova-ui-vuetify/quasar.config.ts
+++ zova-ui-vuetify/quasar.config.ts
@@ -4,12 +4,23 @@
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
 import { configure } from 'quasar/wrappers';
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 export default configure(_ctx => {
   return {
     build: {
-      // extendViteConf(_viteConf) {},
-      // viteVuePluginOptions: {},
+      extendViteConf(viteConf) {
+        // plugins
+        viteConf.plugins = viteConf.plugins!.concat([
+          // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+          <any>Vuetify({
+            autoImport: false,
+          }),
+        ]);
+      },
+      viteVuePluginOptions: {
+        template: { transformAssetUrls },
+      },
     },
 
     devServer: {
