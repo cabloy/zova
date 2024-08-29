0e160c4a77e1f3e431065bbf821b5ccc43da1b54
zhennann <zhen.nann@icloud.com>
2024-08-29 16:32:42 +0800

    feat: model theme

--- zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/resource/beans.ts
+++ zova-ui-quasar/src/suite-vendor/a-quasar/modules/a-quasar/src/resource/beans.ts
@@ -1,8 +1,11 @@
+export * from '../bean/model.theme.js';
 export * from '../bean/tool.themeHandler.js';
 import { ToolThemeHandler } from '../bean/tool.themeHandler.js';
+import { ModelTheme } from '../bean/model.theme.js';
 import 'zova';
 declare module 'zova' {
   export interface IBeanRecord {
     'a-quasar.tool.themeHandler': ToolThemeHandler;
+    'a-quasar.model.theme': ModelTheme;
   }
 }
