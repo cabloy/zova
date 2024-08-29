e2e839290436ca4f6b97abfb3a60270d81b8bc65
zhennann <zhen.nann@icloud.com>
2024-08-29 20:15:12 +0800

    Update main.ts

--- zova-ui-vuetify/src/boot/main.ts
+++ zova-ui-vuetify/src/boot/main.ts
@@ -2,7 +2,6 @@ import { SSRContext } from 'zova';
 import { App } from 'vue';
 import vuetify from './vuetify.js';
 
-import 'roboto-fontface/css/roboto/sass/roboto-fontface.scss';
 import '../css/settings.scss';
 
 export default (app: App, _ssrContext: SSRContext) => {
