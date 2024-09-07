5e43cbed10202854fff06a80ab7c5c3afcecf3ed
zhennann <zhen.nann@icloud.com>
2024-09-07 10:09:36 +0800

    Update cli.tools.res.ts

--- zova-cli/cli-set-front/src/lib/bean/cli.tools.res.ts
+++ zova-cli/cli-set-front/src/lib/bean/cli.tools.res.ts
@@ -33,9 +33,12 @@ export class CliToolsRes extends BeanCliBase {
     const modulePath = module.root;
     await this.helper.ensureDir(path.join(modulePath, 'src/.res'));
     const resDest = path.join(modulePath, 'src/.res/index.ts');
-    // bean
+    // content
     let content = '';
+    // bean
     content += await this._generateBeans(moduleName, modulePath);
+    content += await this._generateComponents(moduleName, modulePath);
+    // save
     await fse.writeFile(resDest, content);
   }
 
@@ -62,6 +65,40 @@ ${contentImports.join('\n')}
 import 'zova';
 declare module 'zova' {
   export interface IBeanRecord {
+    ${contentRecords.join('\n')}
+  }
+}
+`;
+    return content;
+  }
+
+  async _generateComponents(moduleName: string, modulePath: string) {
+    const pattern = `${modulePath}/src/component/*/index.vue`;
+    const files = await eggBornUtils.tools.globbyAsync(pattern);
+    const contentExports: string[] = [];
+    const contentImports: string[] = [];
+    const contentImports2: string[] = [];
+    const contentComponents: string[] = [];
+    const contentRecords: string[] = [];
+    for (const file of files) {
+      const componentName = path.basename(file.substring(0, file.length - '/index.vue'.length));
+      await this.console.log(componentName);
+      const className = componentName.charAt(0).toUpperCase() + componentName.substring(1);
+      const componentFullName = `${moduleName}:${componentName}`;
+      contentExports.push(`export * as NSController${className} from '../component/${componentName}/controller.js';`);
+      contentImports.push(`import * as NSController${className} from '../component/${componentName}/controller.js';`);
+      contentImports2.push(`import ${componentName} from '../component/${componentName}/index.vue';`);
+      contentComponents.push(componentName);
+      contentRecords.push(`'${componentFullName}': NSController${className}.Controller${className};`);
+    }
+    // combine
+    const content = `${contentExports.join('\n')}
+${contentImports.join('\n')}
+${contentImports2.join('\n')}
+export const components = { ${contentComponents.join(', ')} };
+import 'zova';
+declare module 'zova' {
+  export interface IComponentRecord {
     ${contentRecords.join('\n')}
   }
 }
