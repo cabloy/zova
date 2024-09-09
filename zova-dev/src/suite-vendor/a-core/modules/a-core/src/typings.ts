9f349316159855d12fddaba9e87d4208c4f4f303
zhennann <zhen.nann@icloud.com>
2024-09-09 16:23:21 +0800

    feat: remove typings.ts

--- zova-dev/src/suite-vendor/a-core/modules/a-core/src/typings.ts
+++
@@ -1,5 +0,0 @@
-export type * as APinia from 'zova-module-a-pinia';
-export type * as ARouter from 'zova-module-a-router';
-export type * as AModel from 'zova-module-a-model';
-export type * as AStyle from 'zova-module-a-style';
-export type * as ATabs from 'zova-module-a-tabs';
