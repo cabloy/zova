435b29d1af3c26e7a91351732b04c00d97f9717b
zhennann <zhen.nann@icloud.com>
2024-09-09 16:41:29 +0800

    feat: remove api

--- zova-dev/src/suite-vendor/a-core/modules/a-pinia/src/index.ts
+++ zova-dev/src/suite-vendor/a-core/modules/a-pinia/src/index.ts
@@ -1,7 +1,6 @@
 import './assets/css/module.scss';
 export * from './config/index.js';
 export * from './resource/index.js';
-export * from './api/index.js';
 
 export * from './types.js';
 export * from './monkey.js';
--- zova-dev/src/suite/a-home/modules/home-user/src/index.ts
+++ zova-dev/src/suite/a-home/modules/home-user/src/index.ts
@@ -2,5 +2,4 @@ export * from './.metadata/index.js';
 import './assets/css/module.scss';
 export * from './config/index.js';
 export * from './resource/index.js';
-export * from './api/index.js';
 export * from './routes.js';
