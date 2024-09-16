708a1358d6beb8bc12e293dcb5d9884b8d044eaf
zhennann <zhen.nann@icloud.com>
2024-09-16 20:13:13 +0800

    feat: generic

---
+++ zova-cli/cli-set-front/cli/templates/refactor/componentGeneric/snippets/index.cjs
@@ -0,0 +1,18 @@
+const __snippet_export = `export const ParamsSchema = zz.object({});
+export type ParamsInput = zz.input<typeof ParamsSchema>;
+export type ParamsOutput = zz.output<typeof ParamsSchema>;\n`;
+const __snippet_type = '@Local() export class $_$0 extends $_$1<$$$1> {$$$2}';
+
+module.exports = {
+  file: 'controller.ts',
+  parseOptions: { language: 'plain' },
+  async transform({ ast }) {
+    if (ast.includes('generic="T"')) return;
+    ast = ast
+      .replace('<script setup lang="ts">', <script setup lang="ts" generic="T"></script>)
+      .replace('defineProps<Props>', 'defineProps<Props<T>>')
+      .replace('defineEmits<Emits>', 'defineEmits<Emits<T>>');
+    // ok
+    return ast;
+  },
+};
