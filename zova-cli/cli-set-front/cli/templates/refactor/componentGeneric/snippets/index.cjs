f6ab6263a134e057de63c8d7ae22216ff601443f
zhennann <zhen.nann@icloud.com>
2024-09-16 20:17:36 +0800

    Update index.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/componentGeneric/snippets/index.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/componentGeneric/snippets/index.cjs
@@ -4,12 +4,12 @@ export type ParamsOutput = zz.output<typeof ParamsSchema>;\n`;
 const __snippet_type = '@Local() export class $_$0 extends $_$1<$$$1> {$$$2}';
 
 module.exports = {
-  file: 'controller.ts',
+  file: 'index.vue',
   parseOptions: { language: 'plain' },
   async transform({ ast }) {
     if (ast.includes('generic="T"')) return;
     ast = ast
-      .replace('<script setup lang="ts">', <script setup lang="ts" generic="T"></script>)
+      .replace('<script setup lang="ts">', '<script setup lang="ts" generic="T">')
       .replace('defineProps<Props>', 'defineProps<Props<T>>')
       .replace('defineEmits<Emits>', 'defineEmits<Emits<T>>');
     // ok
