d6b24c014f3333f0ae823cf0f40e863d3487537f
zhennann <zhen.nann@icloud.com>
2024-09-17 09:15:01 +0800

    feat: another style

---
+++ zova-cli/cli-set-front/cli/templates/refactor/anotherStyle/snippets/render.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  file: 'render.tsx',
+  parseOptions: { language: 'plain' },
+  async transform({ ast, argv }) {
+    const res = ast.match(/import {.*? Use[ ,].*?} from 'zova';/);
+    if (!res) {
+      ast = ast.replace(" } from 'zova';", ", Use } from 'zova';");
+    }
+    ast = ast
+      .replace(
+        "import { ScopeModule } from '../../.metadata/this.js';",
+        `import { ScopeModule } from '../../.metadata/this.js';\nimport { ${argv.renderNameCapitalize} } from './${argv.renderName}.jsx';`,
+      )
+      .replace(
+        'extends BeanRenderBase<ScopeModule> {',
+        `extends BeanRenderBase<ScopeModule> {\n  @Use()\n  $$${argv.renderName}: ${argv.renderNameCapitalize};\n`,
+      );
+    // ok
+    return ast;
+  },
+};
