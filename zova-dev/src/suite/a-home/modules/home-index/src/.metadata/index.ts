7a1900a50e39a57768a2d355a8446a16851523df
zhennann <zhen.nann@icloud.com>
2024-09-18 11:43:14 +0800

    feat: home

--- zova-dev/src/suite/a-home/modules/home-index/src/.metadata/index.ts
+++ zova-dev/src/suite/a-home/modules/home-index/src/.metadata/index.ts
@@ -13,31 +13,18 @@ declare module 'zova' {
 export const pagePathSchemas = {};
 export const pageNameSchemas = {};
 /** pages: end */
-/** locale: begin */
-import locale_en_us from '../config/locale/en-us.js';
-import locale_zh_cn from '../config/locale/zh-cn.js';
-export const locales = {
-  'en-us': locale_en_us,
-  'zh-cn': locale_zh_cn,
-};
-/** locale: end */
 /** scope: begin */
-import { BeanScopeBase, Scope, TypeLocaleBase, TypeModuleResource } from 'zova';
+import { BeanScopeBase, Scope, TypeModuleResource } from 'zova';
 
 @Scope()
 export class ScopeModuleHomeIndex extends BeanScopeBase {}
 
-export interface ScopeModuleHomeIndex
-  extends TypeModuleResource<any, any, any, (typeof locales)[TypeLocaleBase], any, any> {}
+export interface ScopeModuleHomeIndex extends TypeModuleResource<any, any, any, any, any, any> {}
 
 import 'zova';
 declare module 'zova' {
   export interface IBeanScopeRecord {
     'home-index': ScopeModuleHomeIndex;
-  }
-
-  export interface IBeanScopeLocale {
-    'home-index': (typeof locales)[TypeLocaleBase];
   }
 }
 /** scope: end */
