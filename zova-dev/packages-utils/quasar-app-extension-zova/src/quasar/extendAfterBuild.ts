cb41240bae9a86931bcbf5c592aa960b0d98fdac
zhennann <zhen.nann@icloud.com>
2024-09-19 18:46:42 +0800

    feat: patchPackage

--- zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendAfterBuild.ts
+++ zova-dev/packages-utils/quasar-app-extension-zova/src/quasar/extendAfterBuild.ts
@@ -1,20 +1,38 @@
-import fse from 'fs-extra';
 import { QuasarConf } from '@quasar/app-vite/types/configuration/conf.js';
 import { IndexAPI } from '@quasar/app-vite';
 import path from 'node:path';
 import { ConfigContext } from './types.js';
+import { loadJSONFile, saveJSONFile } from '../utils.js';
 
 export function extendAfterBuild(context: ConfigContext, _flavor: string) {
   return async function extendAfterBuild(conf: QuasarConf, api: IndexAPI) {
     const appMode = api.ctx.modeName;
     if (appMode === 'ssr') {
       // env
-      const env = Object.assign({}, context.zovaViteMeta?.env, {
-        SERVER: true,
-        CLIENT: false,
-      });
-      const envFile = path.join(conf.build!.distDir!, '.env.json');
-      fse.outputFileSync(envFile, JSON.stringify(env, null, 2));
+      await generateEnvJson(conf);
+      // package.json
+      await patchPackage(conf);
     }
   };
+
+  async function generateEnvJson(conf: QuasarConf) {
+    const env = Object.assign({}, context.zovaViteMeta?.env, {
+      SERVER: true,
+      CLIENT: false,
+    });
+    const envFile = path.join(conf.build!.distDir!, '.env.json');
+    await saveJSONFile(envFile, env);
+  }
+
+  async function patchPackage(conf: QuasarConf) {
+    const pkgFile = path.join(conf.build!.distDir!, 'package.json');
+    const pkg = await loadJSONFile(pkgFile);
+    const deps = pkg.dependencies;
+    for (const key in deps) {
+      if (key.startsWith('zova-module-')) {
+        delete deps[key];
+      }
+    }
+    await saveJSONFile(pkgFile, pkg);
+  }
 }
