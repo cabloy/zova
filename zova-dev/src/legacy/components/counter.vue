b660d5cbc8ce1df47a19b19035b509bce74b3044
zhennann <zhen.nann@icloud.com>
2024-09-20 15:04:57 +0800

    feat: legacy

--- zova-dev/src/legacy/components/counter.vue
+++ zova-dev/src/legacy/components/counter.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { useCounter } from '@/composables/counter.js';
+import { useCounter } from '@/composables/counter';
 const { count, increment, decrement } = useCounter();
 </script>
 <template>
   <div>
-    <div>{{ count }}</div>
-    <button @click="increment">Increment</button>
-    <button @click="decrement">Decrement</button>
+    <div>Legacy Vue3 composables/components can be used directly in Zova</div>
+    <div>{{ `count: ${count}` }}</div>
+    <button class="btn btn-primary" @click="increment">Increment</button>
+    <button class="btn btn-secondary" @click="decrement">Decrement</button>
   </div>
 </template>
--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/legacy/render.tsx
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/legacy/render.tsx
@@ -1,12 +1,17 @@
 import { BeanRenderBase, Local } from 'zova';
 import type { StyleLegacy } from './style.js';
 import { ScopeModule } from '../../.metadata/this.js';
+import Counter from '@/components/counter.vue';
 
 export interface RenderLegacy extends StyleLegacy {}
 
 @Local()
 export class RenderLegacy extends BeanRenderBase<ScopeModule> {
   render() {
-    return <div></div>;
+    return (
+      <this.$component.page>
+        <Counter></Counter>
+      </this.$component.page>
+    );
   }
 }
