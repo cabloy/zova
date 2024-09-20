16ea62afc760b5b4d25840f8f4cc818520c94125
zhennann <zhen.nann@icloud.com>
2024-09-20 20:52:04 +0800

    feat: remove home-router

--- zova-dev/src/suite/a-home/modules/home-base/src/monkey.ts
+++ zova-dev/src/suite/a-home/modules/home-base/src/monkey.ts
@@ -1,11 +1,17 @@
 import { BeanBase, BeanContainer, BeanSimple, IMonkeyAppInitialize, IMonkeyBeanInit } from 'zova';
 import axios from 'axios';
 import { BeanApi } from './bean/bean.api.js';
+import { LocalRouter } from './bean/local.router.js';
 
 export class Monkey extends BeanSimple implements IMonkeyAppInitialize, IMonkeyBeanInit {
+  localRouter: LocalRouter;
+
   async appInitialize() {
+    // api
     this.app.meta.$axios = axios;
     this.app.meta.$api = (await this.bean._getBean('home-base.bean.api', false)) as BeanApi;
+    // router
+    this.localRouter = await this.bean._newBean(LocalRouter, false);
   }
   async beanInit(bean: BeanContainer, beanInstance: BeanBase) {
     const self = this;
