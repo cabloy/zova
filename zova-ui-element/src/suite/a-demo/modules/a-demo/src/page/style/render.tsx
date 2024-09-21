682caab853a26350675dfcfa430505b2d25e1f92
zhennann <zhen.nann@icloud.com>
2024-09-21 09:49:49 +0800

    feat: home-base

--- zova-ui-element/src/suite/a-demo/modules/a-demo/src/page/style/render.tsx
+++ zova-ui-element/src/suite/a-demo/modules/a-demo/src/page/style/render.tsx
@@ -30,7 +30,7 @@ export class RenderStyle extends BeanRenderBase<ScopeModule> {
         <div>
           <div style={{ color: this.$token['color-primary'] }}>theme: {this.$theme.name}</div>
           <ElRadioGroup v-model={this.$theme.name}>
-            <ElRadio value={getBeanName('home-theme.theme.default')}>Default</ElRadio>
+            <ElRadio value={getBeanName('home-base.theme.default')}>Default</ElRadio>
             <ElRadio value={getBeanName('a-demo.theme.orange')}>Orange</ElRadio>
           </ElRadioGroup>
         </div>
--- zova-ui-element/src/suite/a-home/modules/home-base/src/bean/theme.default.ts
+++ zova-ui-element/src/suite/a-home/modules/home-base/src/bean/theme.default.ts
@@ -1,25 +1,14 @@
 import { BeanBase, Theme } from 'zova';
 import { ScopeModule } from '../.metadata/this.js';
 import { ThemeApplyParams, ThemeApplyResult, ThemeBase } from 'zova-module-a-style';
-import { ThemeToken } from 'zova-module-a-devui';
+import { ThemeToken } from 'zova-module-a-element';
+import { themeDark } from './theme/themeDark.js';
+import { themeLight } from './theme/themeLight.js';
 
 @Theme()
 export class ThemeDefault extends BeanBase<ScopeModule> implements ThemeBase {
   async apply({ name: _name, dark }: ThemeApplyParams): Promise<ThemeApplyResult> {
-    const token: ThemeToken = {
-      color: {
-        primary: '#1976d2',
-      },
-      var: {
-        borderColor: '#297acc',
-      },
-      component: {
-        page: {
-          background: dark ? '#121212' : '#fff',
-          color: dark ? '#fff' : '#000',
-        },
-      },
-    };
+    const token: ThemeToken = dark ? themeDark : themeLight;
     return { token };
   }
 }
