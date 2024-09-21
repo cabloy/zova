db791e4e7f66a7daed27e099545effc76e19d0e8
zhennann <zhen.nann@icloud.com>
2024-09-21 11:58:53 +0800

    Update monkey.ts

--- zova-dev/src/suite-vendor/a-devui/modules/a-devui/src/monkey.ts
+++ zova-dev/src/suite-vendor/a-devui/modules/a-devui/src/monkey.ts
@@ -1,16 +1,10 @@
-import { BeanBase, BeanContainer, BeanSimple, IMonkeySystem } from 'zova';
+import { BeanSimple, IMonkeyAppInitialize } from 'zova';
 import { ScopeModuleAStyle } from 'zova-module-a-style';
 
-export class Monkey extends BeanSimple implements IMonkeySystem {
+export class Monkey extends BeanSimple implements IMonkeyAppInitialize {
   async appInitialize() {
     // defaultThemeHandler
     const scopeStyle: ScopeModuleAStyle = await this.bean.getScope('a-style');
     scopeStyle.config.defaultThemeHandler = 'a-devui.tool.themeHandler';
   }
-  async appInitialized() {}
-  async appReady() {}
-  async beanInit(_bean: BeanContainer, _beanInstance: BeanBase) {}
-  async beanInited(_bean: BeanContainer, _beanInstance: BeanBase) {}
-  beanDispose(_bean: BeanContainer, _beanInstance: BeanBase) {}
-  beanDisposed(_bean: BeanContainer, _beanInstance: BeanBase) {}
 }
