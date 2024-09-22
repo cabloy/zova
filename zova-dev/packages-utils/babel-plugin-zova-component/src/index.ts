a947eb899605da27f7b29dee680c944ed3903312
zhennann <zhen.nann@icloud.com>
2024-09-22 15:16:38 +0800

    Update index.ts

--- zova-dev/packages-utils/babel-plugin-zova-component/src/index.ts
+++ zova-dev/packages-utils/babel-plugin-zova-component/src/index.ts
@@ -2,6 +2,11 @@ import { /*template,*/ types as t, type PluginPass } from '@babel/core';
 import type { NodePath, Visitor } from '@babel/traverse';
 import { parseInfo } from '@cabloy/module-info';
 
+interface componentInfo {
+  importName: string;
+  localName: string;
+}
+
 export default function () {
   const visitor: Visitor<PluginPass> = {
     Program(path: NodePath<t.Program>) {
@@ -11,9 +16,6 @@ export default function () {
       };
       // traverse
       path.traverse(createVisitor(context));
-      if (context.imports.length > 0) {
-        console.log(context.imports);
-      }
     },
   };
   return { visitor };
@@ -24,7 +26,7 @@ function createVisitor(context) {
     ImportDeclaration(path: NodePath<t.ImportDeclaration>) {
       const moduleFullName = path.node.source.value;
       if (!moduleFullName.startsWith('zova-module-')) return;
-      const components: string[] = [];
+      const components: componentInfo[] = [];
       for (const specifier of path.node.specifiers) {
         if (
           !t.isImportSpecifier(specifier) ||
@@ -34,7 +36,10 @@ function createVisitor(context) {
         ) {
           continue;
         }
-        components.push(specifier.imported.name);
+        components.push({
+          importName: specifier.imported.name,
+          localName: specifier.local.name,
+        });
       }
       if (components.length > 0) {
         const _import = {
@@ -59,18 +64,3 @@ function isZComponent(name: string) {
 function isUpperCase(character) {
   return /^[A-Z]$/.test(character);
 }
-
-// function findImports(path: NodePath<t.Program>) {
-//   //const _imports = [];
-//   for (const node of path.node.body) {
-//     if (!t.isImportDeclaration(node) || !node.source.value.startsWith('zova-module-')) continue;
-//     //const _import = {};
-//     for (const specifier of node.specifiers) {
-//       if (!t.isImportSpecifier(specifier) || specifier.importKind !== 'value' || !t.isIdentifier(specifier.imported)) {
-//         continue;
-//       }
-//       const specifierName = specifier.imported.name;
-//       console.log(specifierName);
-//     }
-//   }
-// }
