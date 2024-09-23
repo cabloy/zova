d0ba1b87e2bea5bbd7d7b3daf9f95f8dded85dbc
zhennann <zhen.nann@icloud.com>
2024-09-23 20:31:11 +0800

    feat: remove app for antdv

--- zova-ui-antdv/src/suite/a-home/modules/home-layout/src/component/layoutEmpty/render.tsx
+++ zova-ui-antdv/src/suite/a-home/modules/home-layout/src/component/layoutEmpty/render.tsx
@@ -1,6 +1,6 @@
 import { BeanRenderBase, Local } from 'zova';
 import type { ControllerLayoutEmpty } from './controller.js';
-import { App, ConfigProvider, StyleProvider } from 'ant-design-vue';
+import { ConfigProvider, StyleProvider } from 'ant-design-vue';
 import { RouterView } from 'vue-router';
 
 export interface RenderLayoutEmpty extends ControllerLayoutEmpty {}
@@ -11,9 +11,7 @@ export class RenderLayoutEmpty extends BeanRenderBase {
     return (
       <StyleProvider cache={this.$antdvStyleCache}>
         <ConfigProvider>
-          <App>
-            <RouterView />
-          </App>
+          <RouterView />
         </ConfigProvider>
       </StyleProvider>
     );
