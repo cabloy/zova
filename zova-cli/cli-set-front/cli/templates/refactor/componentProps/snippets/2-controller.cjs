557298ad06d1faefe6c39316b16812e6be234cbd
zhennann <zhen.nann@icloud.com>
2024-09-26 17:26:14 +0800

    Update 2-controller.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/componentProps/snippets/2-controller.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/componentProps/snippets/2-controller.cjs
@@ -7,16 +7,23 @@ module.exports = {
   file: 'controller.ts',
   parseOptions: { language: 'plain' },
   async transform({ ast }) {
-    if (ast.includes('export interface Props<')) return;
-    const matchController = ast.match(/export class (.*?) extends/);
+    if (ast.includes('export interface Props')) throw new Error('Props exists');
+    const matchController = ast.match(/export class ([^< ]*)(.*?) extends/);
     const className = matchController[1];
-    ast = ast
-      .replace('export interface Props', 'export interface Props<T>')
-      .replace(`${className}, Slots`, `${className}<T>, Slots<T>`)
-      .replace('export type Emits', 'export type Emits<_T>')
-      .replace('export interface Slots', 'export interface Slots<_T>')
-      .replace(`export class ${className}`, `export class ${className}<T = unknown>`)
-      .replace('ScopeModule, Props, Emits, Slots', 'ScopeModule, Props<T>, Emits<T>, Slots<T>');
+    const hasGeneric = !!matchController[2];
+    const genericT = hasGeneric ? '<T>' : '';
+    const hasSlots = ast.includes('export interface Slots');
+    // PropsBase
+    if (!ast.match(/import \{[^\}]*PropsBase[^\}]*\} from 'zova';/)) {
+      ast = ast.replace(/import \{ ([^\}]*) \} from 'zova';/, (_, $1) => {
+        return `import { ${$1}, PropsBase } from 'zova';`;
+      });
+    }
+    ast = ast.replace(
+      '@Local',
+      `export interface Props${genericT} extends PropsBase<${className}${genericT}${hasSlots ? `, Slots${genericT}` : ''}> {}\n\n@Local`,
+    );
+
     // ok
     return ast;
   },
