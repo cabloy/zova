24b11fcf30b4ddfd2ba332c8ec24fed3b0dae9a0
zhennann <zhen.nann@icloud.com>
2024-09-26 17:08:05 +0800

    Update 1-index.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/componentProps/snippets/1-index.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/componentProps/snippets/1-index.cjs
@@ -7,11 +7,22 @@ module.exports = {
   file: 'index.vue',
   parseOptions: { language: 'plain' },
   async transform({ ast }) {
-    if (ast.includes('generic="T"')) return;
+    if (!ast.includes(', PropsBase')) throw new Error('Props exists');
+
+    const hasGeneric = ast.match(/<script[^/]*?generic="[^"]*?"[^/]*?>/);
+    const genericT = hasGeneric ? '<T>' : '';
+    //
+    const matchController = ast.match(/import \{ (Controller[^, ]*)/);
+    const className = matchController[1];
     ast = ast
-      .replace('<script setup lang="ts">', '<script setup lang="ts" generic="T">')
-      .replace('defineProps<Props>', 'defineProps<Props<T>>')
-      .replace('defineEmits<Emits>', 'defineEmits<Emits<T>>');
+      .replace(', PropsBase', '')
+      .replace(/import \{ ([^\}]*) \} from '.\/controller.js';/, (_, $1) => {
+        return `import { ${$1}, Props } from './controller.js';`;
+      })
+      .replace(
+        /const props = [^;]*;/,
+        `const props = withDefaults(defineProps<Props${genericT}>(), ${className}.$propsDefault);`,
+      );
     // ok
     return ast;
   },
