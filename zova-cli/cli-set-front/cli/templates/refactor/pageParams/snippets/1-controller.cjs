8d2bdc1033e25c0cd39c512d2a97117dbd7323de
zhennann <zhen.nann@icloud.com>
2024-09-26 19:44:22 +0800

    Update 1-controller.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/pageParams/snippets/1-controller.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/pageParams/snippets/1-controller.cjs
@@ -1,35 +1,28 @@
 const __snippet_export = `export const ParamsSchema = zz.object({});
 export type ParamsInput = zz.input<typeof ParamsSchema>;
 export type ParamsOutput = zz.output<typeof ParamsSchema>;\n`;
-const __snippet_type = '@Local() export class $_$0 extends $_$1<$$$1> {$$$2}';
 
 module.exports = {
   file: 'controller.ts',
-  async transform({ cli, ast }) {
+  parseOptions: { language: 'plain' },
+  async transform({ ast }) {
     // check if exists
-    if (ast.has('export type ParamsInput = zz.input<typeof ParamsSchema>')) {
-      return;
+    if (ast.includes('export type ParamsInput')) throw new Error('Params exists');
+    // zz
+    if (!ast.match(/import \{[^\}]*zz[^\}]*\} from 'zova';/)) {
+      ast = ast.replace(/import \{ ([^\}]*) \} from 'zova';/, (_, $1) => {
+        return `import { ${$1}, zz } from 'zova';`;
+      });
     }
     // export
-    const code = await cli.template.renderContent({ content: __snippet_export });
-    ast.find('@Local() export class $_$0 {}').before(code);
-    // zz
-    if (!ast.has("import { $$$0, zz } from 'zova'")) {
-      ast.replace("import { $$$0 } from 'zova'", "import { $$$0, zz } from 'zova'");
-    }
-    // type
-    const res = ast.find(__snippet_type);
-    const typeNames = res.match['$$$1'].map(item => item.typeName?.name);
-    if (!typeNames[0]) typeNames[0] = 'ScopeModule';
-    if (!typeNames[1]) typeNames[1] = 'unknown';
-    if (!typeNames[2]) typeNames[2] = 'ParamsOutput';
-    ast.replace(
-      __snippet_type,
-      __snippet_type
-        .replace('$_$0', res.match[0][0].value)
-        .replace('$_$1', res.match[1][0].value)
-        .replace('$$$1', typeNames.join(', ')),
-    );
+    ast = ast.replace('@Local', `${__snippet_export}\n@Local`);
+    // BeanControllerPageBase
+    ast = ast.replace(/BeanControllerPageBase<(.*?)> \{/, (_, $1) => {
+      const parts = $1.split(',');
+      if (!parts[1]) parts[1] = ' unknown';
+      parts[2] = ' ParamsOutput';
+      return `BeanControllerPageBase<${parts.join(',')}> {`;
+    });
     // ok
     return ast;
   },
