e6cbb2646e735b855aae99a89046fc84309c5b41
zhennann <zhen.nann@icloud.com>
2024-09-26 19:30:12 +0800

    Update 2-controller.cjs

--- zova-cli/cli-set-front/cli/templates/refactor/componentGeneric/snippets/2-controller.cjs
+++ zova-cli/cli-set-front/cli/templates/refactor/componentGeneric/snippets/2-controller.cjs
@@ -10,8 +10,15 @@ module.exports = {
       .replace(`${className}, Slots`, `${className}<T>, Slots<T>`)
       .replace('export type Emits', 'export type Emits<_T>')
       .replace('export interface Slots', 'export interface Slots<_T>')
-      .replace(`export class ${className}`, `export class ${className}<T = unknown>`)
-      .replace('ScopeModule, Props, Emits, Slots', 'ScopeModule, Props<T>, Emits<T>, Slots<T>');
+      .replace(`export class ${className}`, `export class ${className}<T = unknown>`);
+    // BeanControllerBase
+    ast = ast.replace(/BeanControllerBase<(.*?)> \{/, (_, $1) => {
+      const parts = $1.split(',');
+      if (parts[1] === ' Props') parts[1] = ' Props<T>';
+      if (parts[2] === ' Emits') parts[2] = ' Emits<T>';
+      if (parts[3] === ' Slots') parts[3] = ' Slots<T>';
+      return `BeanControllerBase<${parts.join(',')}> {`;
+    });
     // ok
     return ast;
   },
