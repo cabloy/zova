b7f58253e1d0a8682bdcd4fd47cd4ac84bd01b5d
zhennann <zhen.nann@icloud.com>
2024-09-28 17:06:19 +0800

    Update controller.ts

--- zova-dev/src/suite/a-demo/modules/a-demo/src/page/pinia/controller.ts
+++ zova-dev/src/suite/a-demo/modules/a-demo/src/page/pinia/controller.ts
@@ -1,6 +1,6 @@
 import { BeanControllerPageBase, Local, Use, zz } from 'zova';
 import { ScopeModule } from '../../.metadata/this.js';
-import type { StoreCounter } from '../../bean/store.counter.js';
+import { StoreCounter } from '../../bean/store.counter.js';
 
 export const ParamsSchema = zz.object({});
 export type ParamsInput = zz.input<typeof ParamsSchema>;
@@ -12,7 +12,7 @@ export type QueryOutput = zz.output<typeof QuerySchema>;
 
 @Local()
 export class ControllerPagePinia extends BeanControllerPageBase<ScopeModule, QueryOutput, ParamsOutput> {
-  @Use('a-demo.store.counter')
+  @Use()
   $$counter: StoreCounter;
 
   protected async __init__() {}
