f07813274af33096b2c6b080dd059aa14686ee48
zhennann <zhen.nann@icloud.com>
2024-09-28 10:07:05 +0800

    Update index.ts

--- zova-dev/packages-utils/babel-plugin-zova-bean-use/src/index.ts
+++ zova-dev/packages-utils/babel-plugin-zova-bean-use/src/index.ts
@@ -7,11 +7,6 @@ interface ComponentInfo {
   localName: string;
 }
 
-// interface ComponentFindInfo {
-//   import: ImportInfo;
-//   component: ComponentInfo;
-// }
-
 interface ImportInfo {
   moduleFullName: string;
   moduleInfo: IModuleInfo;
@@ -33,12 +28,6 @@ export default function () {
       // traverse
       path.traverse(createVisitor(context));
       if (context.imports.length === 0) return;
-      // insertComponents
-      insertComponents(path, context);
-      // insertImport
-      insertImport(path);
-      // removeImports
-      removeImports(context);
     },
   };
   return { visitor };
@@ -55,10 +44,12 @@ function createVisitor(context: ContextInfo) {
           !t.isImportSpecifier(specifier) ||
           specifier.importKind !== 'value' ||
           !t.isIdentifier(specifier.imported) ||
-          !isZComponent(specifier.imported.name)
+          isZComponent(specifier.imported.name) ||
+          !isUpperCase(specifier.imported.name[0])
         ) {
           continue;
         }
+        console.log(specifier.imported.name);
         components.push({
           importName: specifier.imported.name,
           localName: specifier.local.name,
@@ -83,49 +74,4 @@ function isZComponent(name: string) {
 
 function isUpperCase(character) {
   return /^[A-Z]$/.test(character);
-}
-
-function removeImports(context: ContextInfo) {
-  for (const _import of context.imports) {
-    for (const component of _import.components) {
-      const index = _import.path.node.specifiers.findIndex(item => item.local.name === component?.localName);
-      _import.path.node.specifiers.splice(index, 1);
-    }
-    if (_import.path.node.specifiers.length === 0) {
-      _import.path.remove();
-    }
-  }
-}
-
-function insertImport(path: NodePath<t.Program>) {
-  const nodeImport = t.importDeclaration(
-    [t.importSpecifier(t.identifier('__z_createZovaComponent'), t.stringLiteral('createZovaComponent'))],
-    t.stringLiteral('zova'),
-  );
-  path.get('body')[0].insertBefore(nodeImport);
-}
-
-function insertComponents(path: NodePath<t.Program>, context: ContextInfo) {
-  for (const _import of context.imports) {
-    for (const component of _import.components) {
-      const nodeComponent = t.variableDeclaration('const', [
-        t.variableDeclarator(
-          t.identifier(component.localName),
-          t.callExpression(t.identifier('__z_createZovaComponent'), [
-            t.stringLiteral(_import.moduleInfo.relativeName),
-            t.stringLiteral(parseRealComponentName(component.importName)),
-          ]),
-        ),
-      ]);
-      path.get('body')[0].insertBefore(nodeComponent);
-    }
-  }
-}
-
-function parseRealComponentName(name: string) {
-  return firstCharToLowerCase(name.substring(1));
-}
-
-function firstCharToLowerCase(name: string) {
-  return name.charAt(0).toLowerCase() + name.substring(1);
 }
