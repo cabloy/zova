fd0731ce9abbb734e6e1e7f254fcc79290b9e9ee
zhennann <zhen.nann@icloud.com>
2024-10-12 08:32:13 +0800

    Update index.ts

--- zova-dev/packages-utils/mutate-on-copy/src/index.ts
+++ zova-dev/packages-utils/mutate-on-copy/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export function mutate<T>(target: T, fn: (copy: T) => T | undefined): T {
+  if (!target) return target;
+  const copy = (Array.isArray(target) ? target.slice() : Object.assign({}, target)) as T;
+  const res = fn(copy);
+  return res === undefined ? copy : res;
+}
