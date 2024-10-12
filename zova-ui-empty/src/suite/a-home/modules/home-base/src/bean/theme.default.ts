6e4e01707eefd519cc73bc7e0c651c85830e0fb3
zhennann <zhen.nann@icloud.com>
2024-10-12 19:52:31 +0800

    refactor: themeToken

--- zova-ui-empty/src/suite/a-home/modules/home-base/src/bean/theme.default.ts
+++ zova-ui-empty/src/suite/a-home/modules/home-base/src/bean/theme.default.ts
@@ -1,10 +1,12 @@
 import { BeanBase, Theme } from 'zova';
 import { ScopeModule } from '../.metadata/this.js';
 import { ThemeApplyParams, ThemeApplyResult, ThemeBase } from 'zova-module-a-style';
+import { ThemeToken } from '../themeToken.js';
 
 @Theme()
 export class ThemeDefault extends BeanBase<ScopeModule> implements ThemeBase {
   async apply({ name: _name, dark: _dark }: ThemeApplyParams): Promise<ThemeApplyResult> {
-    return {};
+    const token: ThemeToken = {};
+    return { token };
   }
 }
