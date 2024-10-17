5bc7928cb936823b8d6756ab3cae04e1b045ffa1
zhennann <zhen.nann@icloud.com>
2024-10-17 14:59:21 +0800

    Update requiredSome.ts

--- zova-dev/packages-zova/core/src/types/utils/requiredSome.ts
+++ zova-dev/packages-zova/core/src/types/utils/requiredSome.ts
@@ -1,1 +1,2 @@
-export type RequiredSome<T, K extends keyof T> = T & Required<Pick<T, K>>;
+export type RequiredSome<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
+export type PartialSome<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
