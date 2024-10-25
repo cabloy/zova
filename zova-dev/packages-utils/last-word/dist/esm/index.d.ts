7c209d08a261f2105a66dbb7823c6fb58e000d28
zhennann <zhen.nann@icloud.com>
2024-10-25 15:52:14 +0800

    feat: dist

---
+++ zova-dev/packages-utils/last-word/dist/esm/index.d.ts
@@ -0,0 +1,7 @@
+export declare function toLowerCaseFirstChar(str: string): string;
+export declare function parseLastWord(str?: string, toLowerCase?: boolean): string | undefined;
+export declare function parseFirstWord(str?: string, toLowerCase?: boolean): string | undefined;
+export declare function skipPrefix(str?: string, prefix?: string, toLowerCase?: boolean): string | undefined;
+export declare function skipLastWord(str?: string, lastWord?: string, toLowerCase?: boolean): string | undefined;
+export declare function splitWords(str?: string, toLowerCase?: boolean, separator?: string): string | undefined;
+//# sourceMappingURL=index.d.ts.map
---
+++ zova-dev/packages-utils/last-word/dist/esm/index.mjs
@@ -0,0 +1,91 @@
+function _parseLastWord(str) {
+    if (!str)
+        return str;
+    for (let i = str.length - 1; i >= 0; i--) {
+        const ch = str.charAt(i);
+        if (ch >= 'A' && ch <= 'Z')
+            return str.substring(i);
+    }
+    return str;
+}
+function _parseFirstWord(str) {
+    if (!str)
+        return str;
+    for (let i = 1; i < str.length; i++) {
+        const ch = str.charAt(i);
+        if (ch >= 'A' && ch <= 'Z')
+            return str.substring(0, i);
+    }
+    return str;
+}
+function toLowerCaseFirstChar(str) {
+    return str.charAt(0).toLowerCase() + str.substring(1);
+}
+function parseLastWord(str, toLowerCase) {
+    const word = _parseLastWord(str);
+    if (!word)
+        return word;
+    return toLowerCase ? toLowerCaseFirstChar(word) : word;
+}
+function parseFirstWord(str, toLowerCase) {
+    const word = _parseFirstWord(str);
+    if (!word)
+        return word;
+    return toLowerCase ? toLowerCaseFirstChar(word) : word;
+}
+function skipPrefix(str, prefix, toLowerCase) {
+    if (!str)
+        return str;
+    let word;
+    if (!prefix) {
+        word = str;
+    }
+    else {
+        const prefix2 = prefix.replace(/\./gi, '');
+        if (str.toLowerCase().startsWith(prefix2.toLowerCase())) {
+            word = str.substring(prefix2.length);
+        }
+        else {
+            word = str;
+        }
+    }
+    return toLowerCase ? toLowerCaseFirstChar(word) : word;
+}
+function skipLastWord(str, lastWord, toLowerCase) {
+    if (!str)
+        return str;
+    if (!lastWord)
+        lastWord = parseLastWord(str);
+    let word;
+    if (str.toLowerCase().endsWith(lastWord.toLowerCase())) {
+        word = str.substring(0, str.length - lastWord.length);
+    }
+    else {
+        word = str;
+    }
+    return toLowerCase ? toLowerCaseFirstChar(word) : word;
+}
+function splitWords(str, toLowerCase, separator = ' ') {
+    if (!str)
+        return str;
+    // parts
+    let parts = [];
+    let pos = str.length;
+    for (let i = str.length - 1; i >= 0; i--) {
+        const ch = str.charAt(i);
+        if (ch >= 'A' && ch <= 'Z') {
+            parts.unshift(str.substring(i, pos));
+            pos = i;
+        }
+    }
+    if (pos > 0)
+        parts.unshift(str.substring(0, pos));
+    // lowerCase
+    if (toLowerCase) {
+        parts = parts.map(item => toLowerCaseFirstChar(item));
+    }
+    // join
+    return parts.join(separator);
+}
+
+export { parseFirstWord, parseLastWord, skipLastWord, skipPrefix, splitWords, toLowerCaseFirstChar };
